5ba8add1794b72948aa0f3baec1b1822a803d678
dufresnesteven <steve.dufresne@automattic.com>
2022-08-05 14:39:11 +0900

    Make string translatable.

--- source/wp-content/themes/wporg-main-2022/src/components/download-modal/DownloadModal.js
+++ source/wp-content/themes/wporg-main-2022/src/components/download-modal/DownloadModal.js
@@ -7,26 +7,31 @@ import { Modal } from '@wordpress/components';
 const DownloadModal = ( { onClose } ) => (
 	<Modal className="wporg__download-modal" title={ __( 'Howdy!', 'wporg' ) } onRequestClose={ onClose }>
 		<>
-			<p className="wporg__download-modal-sub-header is-style-serif">Thanks for downloading WordPress</p>
+			<p className="wporg__download-modal-sub-header is-style-serif">
+				{ __( 'Thanks for downloading WordPress', 'wporg' ) }
+			</p>
 			<div className="wporg__download-modal-content">
 				<p>
-					You&rsquo;re an important part of the global community that has used, built, and transformed
-					the platform into what it is today. Find out more ways you can contribute and make an impact on
-					the future of the web.
+					{ __(
+						"You're an important part of the global community that has used, built, and transformed the platform into what it is today. Find out more ways you can contribute and make an impact on the future of the web.",
+						'wporg'
+					) }
 				</p>
 				<ul>
 					<li>
-						<a href="https://make.wordpress.org/">Get involved in WordPress ↗</a>
+						<a href="https://make.wordpress.org/">{ __( 'Get involved in WordPress ↗', 'wporg' ) }</a>
 					</li>
 					<li>
-						<a href="https://www.meetup.com/pro/wordpress/">Join a local WordPress meetup ↗</a>
+						<a href="https://www.meetup.com/pro/wordpress/">
+							{ __( 'Join a local WordPress meetup ↗', 'wporg' ) }
+						</a>
 					</li>
 					<li>
-						<a href="https://central.wordcamp.org/">Attend a WordCamp ↗</a>
+						<a href="https://central.wordcamp.org/">{ __( 'Attend a WordCamp ↗', 'wporg' ) }</a>
 					</li>
 					<li>
 						<a href="https://wordpressfoundation.org/donate/">
-							Support WordPress and open source education ↗
+							{ __( 'Support WordPress and open source education ↗', 'wporg' ) }
 						</a>
 					</li>
 				</ul>
