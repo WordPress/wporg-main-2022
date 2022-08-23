7daba5124337ed2cb76a1cbaa28b86553e86d789
Kelly Dwan <ryelle@users.noreply.github.com>
2022-08-23 16:09:40 -0400

    Wrap the animation inside a media query check to disable when `prefers-reduced-motion`

--- source/wp-content/themes/wporg-main-2022/src/cycle-headings/view.js
+++ source/wp-content/themes/wporg-main-2022/src/cycle-headings/view.js
@@ -14,18 +14,26 @@ function textFragmentIterator() {
 	};
 }
 
-const getNextTextFragment = textFragmentIterator();
-const targetNode = document.querySelector( '.wp-block-wporg-cycle-heading-container' );
-
-window.setTimeout( () => {
-	targetNode.classList.add( 'is-ready' );
-}, MS_DURATION_PER_ITEM - 1 );
-
-window.setInterval( () => {
-	targetNode.classList.remove( 'fade-in' );
+function init() {
+	const getNextTextFragment = textFragmentIterator();
+	const targetNode = document.querySelector( '.wp-block-wporg-cycle-heading-container' );
 
 	window.setTimeout( () => {
-		targetNode.innerHTML = getNextTextFragment();
-		targetNode.classList.add( 'fade-in' );
-	}, MS_DELAY_BETWEEN_ITEMS );
-}, MS_DURATION_PER_ITEM );
+		targetNode.classList.add( 'is-ready' );
+	}, MS_DURATION_PER_ITEM - 1 );
+
+	window.setInterval( () => {
+		targetNode.classList.remove( 'fade-in' );
+
+		window.setTimeout( () => {
+			targetNode.innerHTML = getNextTextFragment();
+			targetNode.classList.add( 'fade-in' );
+		}, MS_DELAY_BETWEEN_ITEMS );
+	}, MS_DURATION_PER_ITEM );
+}
+
+const wantMotionQuery = window.matchMedia( '(prefers-reduced-motion)' );
+
+if ( ! wantMotionQuery.matches ) {
+	init();
+}
