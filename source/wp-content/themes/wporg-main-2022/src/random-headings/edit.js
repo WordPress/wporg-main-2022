ce5ce6de8e67729c59d1174b23d77647285a28c4
Kelly Dwan <ryelle@users.noreply.github.com>
2022-08-31 13:30:37 -0400

    Block: Add "Random Headings" block

---
+++ source/wp-content/themes/wporg-main-2022/src/random-headings/edit.js
@@ -0,0 +1,18 @@
+/**
+ * WordPress dependencies
+ */
+import { useBlockProps } from '@wordpress/block-editor';
+
+/**
+ * The edit function describes the structure of your block in the context of the
+ * editor. This represents what the editor will render when the block is used.
+ *
+ * @return {WPElement} Element to render.
+ */
+export default function Edit() {
+	return (
+		<h1 { ...useBlockProps() }>
+			WordPress: Publish your <em>passion</em>
+		</h1>
+	);
+}
