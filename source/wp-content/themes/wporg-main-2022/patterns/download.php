d64a014649a5862e9aea2b13f485a51a0ea97c87
Kelly Dwan <ryelle@users.noreply.github.com>
2022-09-06 17:23:06 -0400

    Sync translated files

--- source/wp-content/themes/wporg-main-2022/patterns/download.php
+++ source/wp-content/themes/wporg-main-2022/patterns/download.php
@@ -8,11 +8,11 @@
 ?>
 <!-- wp:group {"layout":{"inherit":true,"type":"constrained"},"align":"full","style":{"spacing":{"padding":{"top":"80px","right":"0px","bottom":"120px","left":"0px"}}}} -->
 <div class="wp-block-group alignfull" style="padding-top:80px;padding-right:0px;padding-bottom:120px;padding-left:0px"><!-- wp:heading {"textAlign":"center","level":1,"fontSize":"heading-1"} -->
-<h1 class="has-text-align-center has-heading-1-font-size">Get WordPress</h1>
+<h1 class="has-text-align-center has-heading-1-font-size"><?php _e( 'Get WordPress', 'wporg' ); ?></h1>
 <!-- /wp:heading -->
 
 <!-- wp:paragraph {"align":"center","className":"is-style-serif","fontSize":"heading-5"} -->
-<p class="has-text-align-center is-style-serif has-heading-5-font-size">Everything you need to set up your site just the way you want it.</p>
+<p class="has-text-align-center is-style-serif has-heading-5-font-size"><?php _e( 'Everything you need to set up your site just the way you want it.', 'wporg' ); ?></p>
 <!-- /wp:paragraph --></div>
 <!-- /wp:group -->
 
@@ -21,25 +21,25 @@
 <div class="wp-block-columns alignwide"><!-- wp:column {"width":"50%","style":{"border":{"right":{"color":"var:preset|color|light-grey-1","width":"1px"}}}} -->
 <div class="wp-block-column" style="border-right-color:var(--wp--preset--color--light-grey-1);border-right-width:1px;flex-basis:50%"><!-- wp:group {"align":"wide","style":{"spacing":{"padding":{"top":"80px","right":"60px","bottom":"80px","left":"0px"}}}} -->
 <div class="wp-block-group alignwide" id="download-install" style="padding-top:80px;padding-right:60px;padding-bottom:80px;padding-left:0px"><!-- wp:heading {"textAlign":"left","fontSize":"heading-4"} -->
-<h2 class="has-text-align-left has-heading-4-font-size">Download and install it yourself</h2>
+<h2 class="has-text-align-left has-heading-4-font-size"><?php _e( 'Download and install it yourself', 'wporg' ); ?></h2>
 <!-- /wp:heading -->
 
 <!-- wp:paragraph {"align":"left","className":"is-style-short-text"} -->
-<p class="has-text-align-left is-style-short-text">For anyone comfortable getting their own hosting and domain.</p>
+<p class="has-text-align-left is-style-short-text"><?php _e( 'For anyone comfortable getting their own hosting and domain.', 'wporg' ); ?></p>
 <!-- /wp:paragraph -->
 
 <!-- wp:buttons {"layout":{"type":"flex","justifyContent":"left"},"style":{"spacing":{"blockGap":"10px"}}} -->
 <div class="wp-block-buttons"><!-- wp:button -->
-<div class="wp-block-button" id="wporg__download-button"><a class="wp-block-button__link wp-element-button" href="[download_link]">Download WordPress [latest_version]</a></div>
+<div class="wp-block-button" id="wporg__download-button"><a class="wp-block-button__link wp-element-button" href="[download_link]"><?php _e( 'Download WordPress [latest_version]', 'wporg' ); ?></a></div>
 <!-- /wp:button -->
 
 <!-- wp:button {"textColor":"blue-1","className":"is-style-outline"} -->
-<div class="wp-block-button is-style-outline"><a class="wp-block-button__link has-blue-1-color has-text-color wp-element-button" href="https://wordpress.org/support/article/how-to-install-wordpress/">Installation guide</a></div>
+<div class="wp-block-button is-style-outline"><a class="wp-block-button__link has-blue-1-color has-text-color wp-element-button" href="https://wordpress.org/support/article/how-to-install-wordpress/"><?php _e( 'Installation guide', 'wporg' ); ?></a></div>
 <!-- /wp:button --></div>
 <!-- /wp:buttons -->
 
 <!-- wp:paragraph {"textColor":"charcoal-4","className":"is-style-short-text","fontSize":"small"} -->
-<p class="is-style-short-text has-charcoal-4-color has-text-color has-small-font-size">Recommend PHP [recommended_php] or greater and MySQL [recommended_mysql] or MariaDB version [recommended_mariadb] or greater.</p>
+<p class="is-style-short-text has-charcoal-4-color has-text-color has-small-font-size"><?php _e( 'Recommend PHP [recommended_php] or greater and MySQL [recommended_mysql] or MariaDB version [recommended_mariadb] or greater.', 'wporg' ); ?></p>
 <!-- /wp:paragraph -->
 
 <!-- wp:navigation {"textColor":"blueberry-1","overlayMenu":"never","className":"is-style-dots","style":{"spacing":{"blockGap":"0px"}},"fontSize":"small"} -->
@@ -57,16 +57,16 @@
 <!-- wp:column {"width":"50%"} -->
 <div class="wp-block-column" style="flex-basis:50%"><!-- wp:group {"align":"wide","style":{"spacing":{"padding":{"top":"80px","right":"0px","bottom":"80px","left":"60px"}}}} -->
 <div class="wp-block-group alignwide" id="hosting" style="padding-top:80px;padding-right:0px;padding-bottom:80px;padding-left:60px"><!-- wp:heading {"textAlign":"left","fontSize":"heading-4"} -->
-<h2 class="has-text-align-left has-heading-4-font-size">Set up with a hosting provider</h2>
+<h2 class="has-text-align-left has-heading-4-font-size"><?php _e( 'Set up with a hosting provider', 'wporg' ); ?></h2>
 <!-- /wp:heading -->
 
 <!-- wp:paragraph {"align":"left","className":"is-style-short-text"} -->
-<p class="has-text-align-left is-style-short-text">For anyone looking for the simplest way to start.</p>
+<p class="has-text-align-left is-style-short-text"><?php _e( 'For anyone looking for the simplest way to start.', 'wporg' ); ?></p>
 <!-- /wp:paragraph -->
 
 <!-- wp:buttons -->
 <div class="wp-block-buttons"><!-- wp:button -->
-<div class="wp-block-button"><a class="wp-block-button__link wp-element-button" href="https://wordpress.org/hosting/">See all recommended hosts</a></div>
+<div class="wp-block-button"><a class="wp-block-button__link wp-element-button" href="https://wordpress.org/hosting/"><?php _e( 'See all recommended hosts', 'wporg' ); ?></a></div>
 <!-- /wp:button --></div>
 <!-- /wp:buttons --></div>
 <!-- /wp:group --></div>
@@ -79,38 +79,38 @@
 <div class="wp-block-columns alignwide are-vertically-aligned-center"><!-- wp:column {"verticalAlignment":"center","width":"50%","style":{"spacing":{"padding":{"right":"60px"}}}} -->
 <div class="wp-block-column is-vertically-aligned-center" style="padding-right:60px;flex-basis:50%"><!-- wp:list {"className":"is-style-features"} -->
 <ul class="is-style-features"><!-- wp:list-item -->
-<li>Simple</li>
+<li><?php _e( 'Simple', 'wporg' ); ?></li>
 <!-- /wp:list-item -->
 
 <!-- wp:list-item -->
-<li>Intuitive</li>
+<li><?php _e( 'Intuitive', 'wporg' ); ?></li>
 <!-- /wp:list-item -->
 
 <!-- wp:list-item -->
-<li>Extendable</li>
+<li><?php _e( 'Extendable', 'wporg' ); ?></li>
 <!-- /wp:list-item -->
 
 <!-- wp:list-item -->
-<li>Free</li>
+<li><?php _e( 'Free', 'wporg' ); ?></li>
 <!-- /wp:list-item -->
 
 <!-- wp:list-item -->
-<li>Open</li>
+<li><?php _e( 'Open', 'wporg' ); ?></li>
 <!-- /wp:list-item --></ul>
 <!-- /wp:list --></div>
 <!-- /wp:column -->
 
 <!-- wp:column {"verticalAlignment":"center","width":"50%","style":{"spacing":{"padding":{"top":"60px","right":"60px","bottom":"60px","left":"60px"}}},"layout":{"inherit":false}} -->
 <div class="wp-block-column is-vertically-aligned-center" style="padding-top:60px;padding-right:60px;padding-bottom:60px;padding-left:60px;flex-basis:50%"><!-- wp:heading {"fontSize":"heading-4"} -->
-<h2 class="has-heading-4-font-size">Powerful right out of the box</h2>
+<h2 class="has-heading-4-font-size"><?php _e( 'Powerful right out of the box', 'wporg' ); ?></h2>
 <!-- /wp:heading -->
 
 <!-- wp:paragraph {"className":"is-style-short-text"} -->
-<p class="is-style-short-text">WordPress combines simplicity for users and publishers with under-the-hood complexity for developers. Discover the features that come standard with WordPress, and extend what the platform can do with the thousands of plugins available.</p>
+<p class="is-style-short-text"><?php _e( 'WordPress combines simplicity for users and publishers with under-the-hood complexity for developers. Discover the features that come standard with WordPress, and extend what the platform can do with the thousands of plugins available.', 'wporg' ); ?></p>
 <!-- /wp:paragraph -->
 
 <!-- wp:paragraph -->
-<p><a href="https://wordpress.org/about/features/">See all WordPress features ↗</a></p>
+<p><?php _e( '<a href="https://wordpress.org/about/features/">See all WordPress features ↗</a>', 'wporg' ); ?></p>
 <!-- /wp:paragraph --></div>
 <!-- /wp:column --></div>
 <!-- /wp:columns --></div>
@@ -120,30 +120,30 @@
 <div class="wp-block-group alignfull has-white-color has-blueberry-1-background-color has-text-color has-background has-link-color" id="resources" style="padding-top:160px;padding-right:0px;padding-bottom:160px;padding-left:0px"><!-- wp:columns {"align":"wide","style":{"spacing":{"blockGap":"120px"}}} -->
 <div class="wp-block-columns alignwide"><!-- wp:column -->
 <div class="wp-block-column"><!-- wp:heading {"fontSize":"heading-2"} -->
-<h2 class="has-heading-2-font-size">You've got WordPress. What's next?</h2>
+<h2 class="has-heading-2-font-size"><?php _e( 'You&#039;ve got WordPress. What&#039;s next?', 'wporg' ); ?></h2>
 <!-- /wp:heading -->
 
 <!-- wp:paragraph {"className":"is-style-short-text"} -->
-<p class="is-style-short-text">If you need help getting started, these resources can help you find your way.</p>
+<p class="is-style-short-text"><?php _e( 'If you need help getting started, these resources can help you find your way.', 'wporg' ); ?></p>
 <!-- /wp:paragraph --></div>
 <!-- /wp:column -->
 
 <!-- wp:column -->
 <div class="wp-block-column"><!-- wp:list {"className":"is-style-links-list"} -->
 <ul class="is-style-links-list"><!-- wp:list-item -->
-<li><a href="https://learn.wordpress.org/course/getting-started-with-wordpress-get-setup/">WordPress courses ↗</a></li>
+<li><a href="https://learn.wordpress.org/course/getting-started-with-wordpress-get-setup/"><?php _e( 'WordPress courses ↗', 'wporg' ); ?></a></li>
 <!-- /wp:list-item -->
 
 <!-- wp:list-item -->
-<li><a href="https://developer.wordpress.org/">Developer resources ↗</a></li>
+<li><a href="https://developer.wordpress.org/"><?php _e( 'Developer resources ↗', 'wporg' ); ?></a></li>
 <!-- /wp:list-item -->
 
 <!-- wp:list-item -->
-<li><a href="https://wordpress.org/support/">WordPress support ↗</a></li>
+<li><a href="https://wordpress.org/support/"><?php _e( 'WordPress support ↗', 'wporg' ); ?></a></li>
 <!-- /wp:list-item -->
 
 <!-- wp:list-item -->
-<li><a href="https://wordpress.org/support/forum/installation/">User forums ↗</a></li>
+<li><a href="https://wordpress.org/support/forum/installation/"><?php _e( 'User forums ↗', 'wporg' ); ?></a></li>
 <!-- /wp:list-item --></ul>
 <!-- /wp:list --></div>
 <!-- /wp:column --></div>
@@ -152,20 +152,20 @@
 
 <!-- wp:group {"align":"wide","style":{"spacing":{"padding":{"top":"120px","right":"0px","bottom":"120px","left":"0px"}}}} -->
 <div class="wp-block-group alignwide" id="mobile" style="padding-top:120px;padding-right:0px;padding-bottom:120px;padding-left:0px"><!-- wp:heading {"textAlign":"center","fontSize":"heading-2"} -->
-<h2 class="has-text-align-center has-heading-2-font-size">Inspiration strikes anywhere</h2>
+<h2 class="has-text-align-center has-heading-2-font-size"><?php _e( 'Inspiration strikes anywhere', 'wporg' ); ?></h2>
 <!-- /wp:heading -->
 
 <!-- wp:paragraph {"align":"center"} -->
-<p class="has-text-align-center">Create and update content on the go with the WordPress app.</p>
+<p class="has-text-align-center"><?php _e( 'Create and update content on the go with the WordPress app.', 'wporg' ); ?></p>
 <!-- /wp:paragraph -->
 
 <!-- wp:group {"layout":{"type":"flex","flexWrap":"nowrap","justifyContent":"center"}} -->
 <div class="wp-block-group"><!-- wp:image {"width":150,"height":45,"linkDestination":"custom"} -->
-<figure class="wp-block-image is-resized"><a href="https://itunes.apple.com/app/apple-store/id335703880?pt=299112&amp;ct=wordpress.org&amp;mt=8"><img src="https://wordpress.org/wp-content/themes/pub/wporg-main/images/badge-apple.png" alt="Download on the Apple App Store" width="150" height="45" /></a></figure>
+<figure class="wp-block-image is-resized"><a href="https://itunes.apple.com/app/apple-store/id335703880?pt=299112&amp;ct=wordpress.org&amp;mt=8"><img src="https://wordpress.org/wp-content/themes/pub/wporg-main/images/badge-apple.png" alt="<?php _e( 'Download on the Apple App Store', 'wporg' ); ?>" width="150" height="45" /></a></figure>
 <!-- /wp:image -->
 
 <!-- wp:image {"width":150,"height":45,"linkDestination":"custom"} -->
-<figure class="wp-block-image is-resized"><a href="http://play.google.com/store/apps/details?id=org.wordpress.android"><img src="https://wordpress.org/wp-content/themes/pub/wporg-main/images/badge-google-play.png" alt="Get it on Google Play" width="150" height="45" /></a></figure>
+<figure class="wp-block-image is-resized"><a href="http://play.google.com/store/apps/details?id=org.wordpress.android"><img src="https://wordpress.org/wp-content/themes/pub/wporg-main/images/badge-google-play.png" alt="<?php _e( 'Get it on Google Play', 'wporg' ); ?>" width="150" height="45" /></a></figure>
 <!-- /wp:image --></div>
 <!-- /wp:group --></div>
 <!-- /wp:group -->
