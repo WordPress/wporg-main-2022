b1ffcd6b6812d1b71af62562126bd54eaf91d619
Kelly Dwan <ryelle@users.noreply.github.com>
2022-10-18 11:35:44 -0400

    Patterns: Sync spacing updates for download pages

--- source/wp-content/themes/wporg-main-2022/patterns/download-counter.php
+++ source/wp-content/themes/wporg-main-2022/patterns/download-counter.php
@@ -6,8 +6,8 @@
  */
 
 ?>
-<!-- wp:group {"align":"full","style":{"elements":{"link":{"color":{"text":"var:preset|color|white"}}},"spacing":{"padding":{"top":"var:preset|spacing|20","right":"var:preset|spacing|20","bottom":"0","left":"var:preset|spacing|20"}},"border":{"top":{"color":"var:preset|color|charcoal-1","width":"1px"},"bottom":{"width":"0px","style":"none"}}},"backgroundColor":"charcoal-2","textColor":"white","className":"is-style-brush-stroke","layout":{"type":"constrained"}} -->
-<div class="wp-block-group alignfull is-style-brush-stroke has-white-color has-charcoal-2-background-color has-text-color has-background has-link-color" style="border-top-color:var(--wp--preset--color--charcoal-1);border-top-width:1px;border-bottom-style:none;border-bottom-width:0px;padding-top:var(--wp--preset--spacing--20);padding-right:var(--wp--preset--spacing--20);padding-bottom:0;padding-left:var(--wp--preset--spacing--20)"><!-- wp:group {"align":"wide","layout":{"type":"flex","flexWrap":"wrap","justifyContent":"space-between"}} -->
+<!-- wp:group {"align":"full","style":{"elements":{"link":{"color":{"text":"var:preset|color|white"}}},"spacing":{"padding":{"top":"16px","right":"var:preset|spacing|20","bottom":"0","left":"var:preset|spacing|20"}},"border":{"top":{"color":"var:preset|color|charcoal-1","width":"1px"},"bottom":{"width":"0px","style":"none"}}},"backgroundColor":"charcoal-2","textColor":"white","className":"is-style-brush-stroke","layout":{"type":"constrained"}} -->
+<div class="wp-block-group alignfull is-style-brush-stroke has-white-color has-charcoal-2-background-color has-text-color has-background has-link-color" style="border-top-color:var(--wp--preset--color--charcoal-1);border-top-width:1px;border-bottom-style:none;border-bottom-width:0px;padding-top:16px;padding-right:var(--wp--preset--spacing--20);padding-bottom:0;padding-left:var(--wp--preset--spacing--20)"><!-- wp:group {"align":"wide","layout":{"type":"flex","flexWrap":"wrap","justifyContent":"space-between"}} -->
 <div class="wp-block-group alignwide"><!-- wp:paragraph {"style":{"typography":{"fontStyle":"normal","fontWeight":"700"}},"fontSize":"normal"} -->
 <p class="has-normal-font-size" style="font-style:normal;font-weight:700"><?php _e( 'Download', 'wporg' ); ?></p>
 <!-- /wp:paragraph -->
@@ -22,6 +22,6 @@
 <h1 class="has-heading-5-font-size" style="font-style:italic"><?php _e( 'Number of WordPress [stable_branch] downloads', 'wporg' ); ?></h1>
 <!-- /wp:heading -->
 
-<!-- wp:wporg/download-counter {"style":{"spacing":{"margin":{"top":"var:preset|spacing|30","bottom":"0"}},"typography":{"fontWeight":"200"}},"textColor":"blueberry-2"} /--></div>
+<!-- wp:wporg/download-counter {"style":{"spacing":{"margin":{"top":"var:preset|spacing|30","bottom":"var:preset|spacing|50"}},"typography":{"fontWeight":"200"}},"textColor":"blueberry-2"} /--></div>
 <!-- /wp:group --></div></div>
 <!-- /wp:cover -->
