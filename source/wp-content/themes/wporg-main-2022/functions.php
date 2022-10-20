1098590d890b94196680fe4439c853433372913d
ren <18050944+renintw@users.noreply.github.com>
2022-10-21 04:39:35 +0800

    Add translator string for preloading required font

--- source/wp-content/themes/wporg-main-2022/functions.php
+++ source/wp-content/themes/wporg-main-2022/functions.php
@@ -66,14 +66,19 @@ function enqueue_assets() {
 
 	// Preload the heading font(s).
 	if ( is_callable( 'global_fonts_preload' ) ) {
-		// TODO: Load required fonts for rosetta sites once they are supported.
-		if ( ! is_rosetta_site() ) {
-			// All headings.
-			global_fonts_preload( 'EB Garamond Latin' );
+		/* translators: Subsets can be any of cyrillic, cyrillic-ext, greek, greek-ext, vietnamese, latin, latin-ext. */
+		$heading_subsets = explode( ',', _x( 'latin', 'Heading font subsets, comma separated', 'wporg' ) );
+		// All headings.
+		foreach ( $heading_subsets as $heading_subset ) {
+			global_fonts_preload( 'EB Garamond', $heading_subset );
+		}
 
-			if ( is_front_page() ) {
-				// The heading on the front-page has some italic.
-				global_fonts_preload( 'EB Garamond Latin italic' );
+		if ( is_front_page() ) {
+			/* translators: Subsets can be any of cyrillic, cyrillic-ext, greek, greek-ext, vietnamese, latin, latin-ext. */
+			$front_page_heading_subsets = explode( ',', _x( 'latin', 'Front page heading font subsets, comma separated', 'wporg' ) );
+			// The heading on the front-page has some italic.
+			foreach ( $front_page_heading_subsets as $front_page_heading_subset ) {
+				global_fonts_preload( 'EB Garamond italic', $front_page_heading_subset );
 			}
 		}
 	}
