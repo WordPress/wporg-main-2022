ab1de46230f0960e6d186acd78b1b402bf1785e0
Kelly Dwan <ryelle@users.noreply.github.com>
2022-10-20 16:28:15 -0400

    Move theme functionality from `main` to `parent` theme
    This functionality will be needed by any theme using the new design, so it should be provided by the parent theme.

--- source/wp-content/themes/wporg-main-2022/functions.php
+++ source/wp-content/themes/wporg-main-2022/functions.php
@@ -19,7 +19,6 @@ require_once( __DIR__ . '/src/release-tables/index.php' );
 add_action( 'wp_enqueue_scripts', __NAMESPACE__ . '\enqueue_assets' );
 add_action( 'init', __NAMESPACE__ . '\register_shortcodes' );
 add_filter( 'render_block_core/pattern', __NAMESPACE__ . '\prevent_arrow_emoji', 20 );
-add_filter( 'render_block_core/pattern', __NAMESPACE__ . '\convert_inline_style_to_rtl', 20 );
 add_filter( 'wp_img_tag_add_loading_attr', __NAMESPACE__ . '\override_lazy_loading', 10, 2 );
 
 /**
@@ -87,23 +86,6 @@ function register_shortcodes() {
 }
 
 /**
- * Make posts and pages available for export from the staging site, so the import script can
- * fetch them to a local dev environment.
- */
-add_filter(
-	'wporg_export_context_post_types',
-	function( $types ) {
-		return array_merge(
-			$types,
-			[
-				'post',
-				'page',
-			]
-		);
-	}
-);
-
-/**
  * Add the variation-selector unicode character to any arrow. This will force
  * the twemoji script to skip these characters, leaving them as text.
  *
@@ -117,47 +99,6 @@ function prevent_arrow_emoji( $content ) {
 	return preg_replace( '/([←↑→↓↔↕↖↗↘↙])/u', '\1&#65038;', $content );
 }
 
-/**
- * Flip inline styles and class names when using RTL display.
- *
- * @param string $content Content of the current post.
- * @return string The updated content.
- */
-function convert_inline_style_to_rtl( $content ) {
-	if ( ! is_rtl() ) {
-		return $content;
-	}
-
-	/*
-	 * Replace initial item with "temp" so that we don't immediately replace it with the original direction.
-	 *
-	 * For example, the transform works like this:
-	 * Start with border-left-color: red; border-right-color: blue;
-	 *         -> border-temp-color: red; border-right-color: blue;
-	 *         -> border-temp-color: red; border-left-color: blue;
-	 *         -> border-right-color: red; border-left-color: blue;
-	 */
-	$replacements = [
-		// Borders.
-		'border-left'  => 'border-temp',
-		'border-right' => 'border-left',
-		'border-temp'  => 'border-right',
-		// Padding
-		'padding-left'  => 'padding-temp',
-		'padding-right' => 'padding-left',
-		'padding-temp'  => 'padding-right',
-		// Margins.
-		'margin-left'  => 'margin-temp',
-		'margin-right' => 'margin-left',
-		'margin-temp'  => 'margin-right',
-		// Text alignment.
-		'has-text-align-left'  => 'has-text-align-temp',
-		'has-text-align-right' => 'has-text-align-left',
-		'has-text-align-temp'  => 'has-text-align-right',
-	];
-
-	return str_replace( array_keys( $replacements ), array_values( $replacements ), $content );
-}
 
 /**
  * Prevents adding loading="lazy" to the editor section's background.
@@ -196,17 +137,5 @@ add_action(
 				'rosetta_main' => 'Rosetta',
 			)
 		);
-	}
-);
-
-/**
- * Increase the "Big image" threshold so our full-size high-DPI-ready images are
- * not scaled down.
- */
-add_filter(
-	'big_image_size_threshold',
-	function() {
-		// 3200 = 2 × 1600px for full-width images on wide screens.
-		return 3200;
 	}
 );
