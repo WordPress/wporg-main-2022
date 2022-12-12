4566584d270d7b57c2e9300a506a44e1e746506e
Kelly Dwan <ryelle@users.noreply.github.com>
2022-12-12 11:09:33 -0500

    Remove `prevent_arrow_emoji` as it's added by the parent theme now

--- source/wp-content/themes/wporg-main-2022/functions.php
+++ source/wp-content/themes/wporg-main-2022/functions.php
@@ -18,8 +18,6 @@ require_once __DIR__ . '/src/release-tables/index.php';
  */
 add_action( 'wp_enqueue_scripts', __NAMESPACE__ . '\enqueue_assets' );
 add_action( 'init', __NAMESPACE__ . '\register_shortcodes' );
-add_filter( 'render_block_core/pattern', __NAMESPACE__ . '\prevent_arrow_emoji', 20 );
-add_filter( 'the_content', __NAMESPACE__ . '\prevent_arrow_emoji', 20 );
 add_filter( 'wp_img_tag_add_loading_attr', __NAMESPACE__ . '\override_lazy_loading', 10, 2 );
 
 /**
@@ -84,21 +82,6 @@ function enqueue_assets() {
 function register_shortcodes() {
 	include __DIR__ . '/inc/shortcodes.php';
 }
-
-/**
- * Add the variation-selector unicode character to any arrow. This will force
- * the twemoji script to skip these characters, leaving them as text.
- *
- * Can be removed once the `wp-exclude-emoji` issue is fixed.
- * See https://core.trac.wordpress.org/ticket/52219.
- *
- * @param string $content Content of the current post.
- * @return string The updated content.
- */
-function prevent_arrow_emoji( $content ) {
-	return preg_replace( '/([←↑→↓↔↕↖↗↘↙])/u', '\1&#65038;', $content );
-}
-
 
 /**
  * Prevents adding loading="lazy" to the editor section's background.
