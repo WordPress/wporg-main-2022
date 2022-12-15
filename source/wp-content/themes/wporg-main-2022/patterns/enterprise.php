aa29995c83a3edc4f22bc8719758cf9e11ecec8a
Adam Wood <adam.wood@automattic.com>
2022-12-15 14:45:14 +1300

    Improve layout and headings hierarchy

--- source/wp-content/themes/wporg-main-2022/patterns/enterprise.php
+++ source/wp-content/themes/wporg-main-2022/patterns/enterprise.php
@@ -7,32 +7,30 @@
 
 ?>
 
-<!-- wp:cover {"overlayColor":"charcoal-2","minHeightUnit":"px","align":"full","style":{"spacing":{"padding":{"top":"0","right":"0","bottom":"0","left":"0"}}}} -->
-<div class="wp-block-cover alignfull" style="padding-top:0;padding-right:0;padding-bottom:0;padding-left:0">
-	<span aria-hidden="true" class="wp-block-cover__background has-charcoal-2-background-color has-background-dim-100 has-background-dim"></span>
-	<div class="wp-block-cover__inner-container">
+<!-- wp:group {"align":"full","backgroundColor":"charcoal-2","className":"wporg-enterprise-header","layout":{"type":"constrained"}} -->
+<div class="wp-block-group alignfull wporg-enterprise-header has-charcoal-2-background-color has-background"><!-- wp:columns {"verticalAlignment":"center","align":"full","style":{"spacing":{"padding":{"top":"var:preset|spacing|90"}}}} -->
+<div class="wp-block-columns alignfull are-vertically-aligned-center" style="padding-top:var(--wp--preset--spacing--90)"><!-- wp:column {"verticalAlignment":"center","width":"55%","style":{"spacing":{"padding":{"left":"var:preset|spacing|10","right":"var:preset|spacing|20"}}}} -->
+<div class="wp-block-column is-vertically-aligned-center" style="padding-right:var(--wp--preset--spacing--20);padding-left:var(--wp--preset--spacing--10);flex-basis:55%"><!-- wp:image {"sizeSlug":"large"} -->
+<figure class="wp-block-image size-large"><img src="https://wordpress.org/files/2022/11/wordpress-enterprise-header-1024x313.png" alt="WordPress Enterprise"/></figure>
+<!-- /wp:image -->
 
-	<!-- wp:media-text {"mediaId":13919,"mediaType":"image","mediaWidth":52,"verticalAlignment":"center","imageFill":false,"style":{"spacing":{"padding":{"top":"var:preset|spacing|90","bottom":"var:preset|spacing|40","left":"var:preset|spacing|10"}}}} -->
-	<div class="wp-block-media-text alignwide is-stacked-on-mobile is-vertically-aligned-center" style="padding-top:var(--wp--preset--spacing--90);padding-bottom:var(--wp--preset--spacing--40);padding-left:var(--wp--preset--spacing--10);grid-template-columns:52% auto">
+<!-- wp:heading {"level":1,"className":"screen-reader-text"} -->
+<h1 class="wp-block-heading screen-reader-text">WordPress Enterprise</h1>
+<!-- /wp:heading --></div>
+<!-- /wp:column -->
 
-	<figure class="wp-block-media-text__media"><img src="https://wordpress.org/files/2022/11/wordpress-enterprise-header-1024x313.png" alt="WordPress Enterprise Heading" class="wp-image-13919 size-full"/></figure><div class="wp-block-media-text__content">
+<!-- wp:column {"verticalAlignment":"center","width":"45%","style":{"spacing":{"padding":{"right":"var:preset|spacing|40","left":"var:preset|spacing|40","bottom":"var:preset|spacing|40","top":"var:preset|spacing|60"}}}} -->
+<div class="wp-block-column is-vertically-aligned-center" style="padding-top:var(--wp--preset--spacing--60);padding-right:var(--wp--preset--spacing--40);padding-bottom:var(--wp--preset--spacing--40);padding-left:var(--wp--preset--spacing--40);flex-basis:45%"><!-- wp:group {"className":"wporg-enterprise-intro","layout":{"type":"flex","flexWrap":"nowrap","justifyContent":"center"}} -->
+<div class="wp-block-group wporg-enterprise-intro"><!-- wp:paragraph {"align":"center","placeholder":"Content…","style":{"typography":{"lineHeight":1.7}},"textColor":"white","className":"is-style-default","fontSize":"small"} -->
+<p class="has-text-align-center is-style-default has-white-color has-text-color has-small-font-size" style="line-height:1.7">WordPress content management system has grown to become a dominant market leader. Discover how today's biggest brands use WordPress, the features that make it so appealing and how you can make WordPress work for your enterprise.</p>
+<!-- /wp:paragraph --></div>
+<!-- /wp:group --></div>
+<!-- /wp:column --></div>
+<!-- /wp:columns -->
 
-	<!-- wp:group {"className":"wporg-enterprise-intro","layout":{"type":"flex","flexWrap":"nowrap","justifyContent":"center"}} -->
-	<div class="wp-block-group wporg-enterprise-intro"><!-- wp:paragraph {"align":"center","placeholder":"Content…","style":{"typography":{"lineHeight":1.7},"spacing":{"margin":{"top":"var:preset|spacing|50"}}},"className":"is-style-default","fontSize":"small"} -->
-	<p class="has-text-align-center is-style-default has-small-font-size" style="margin-top:var(--wp--preset--spacing--50);line-height:1.7">WordPress content management system has grown to become a dominant market leader. Discover how today's biggest brands use WordPress, the features that make it so appealing and how you can make WordPress work for your enterprise.</p>
-	<!-- /wp:paragraph --></div>
-	<!-- /wp:group -->
-
-	</div>
-	</div>
-	<!-- /wp:media-text -->
-	</div>
-</div>
-<!-- /wp:cover -->
-
-<!-- wp:group {"align":"full","backgroundColor":"charcoal-2","textColor":"white","className":"is-style-default","layout":{"type":"constrained"}} -->
-<div class="wp-block-group alignfull is-style-default has-white-color has-charcoal-2-background-color has-text-color has-background"><!-- wp:group {"align":"wide","style":{"spacing":{"blockGap":"var:preset|spacing|50","padding":{"top":"var:preset|spacing|40","bottom":"var:preset|spacing|50"}}},"className":"wporg-enterprise-logos","layout":{"type":"flex","flexWrap":"wrap","justifyContent":"center"}} -->
-<div class="wp-block-group alignwide wporg-enterprise-logos" style="padding-top:var(--wp--preset--spacing--40);padding-bottom:var(--wp--preset--spacing--50)"><!-- wp:image {"align":"center","id":14621,"sizeSlug":"full","linkDestination":"none"} -->
+<!-- wp:group {"align":"full","className":"is-style-default","layout":{"type":"constrained"}} -->
+<div class="wp-block-group alignfull is-style-default"><!-- wp:group {"align":"wide","style":{"spacing":{"blockGap":"var:preset|spacing|50","padding":{"top":"var:preset|spacing|50","bottom":"var:preset|spacing|50"}}},"className":"wporg-enterprise-logos","layout":{"type":"flex","flexWrap":"wrap","justifyContent":"center"}} -->
+<div class="wp-block-group alignwide wporg-enterprise-logos" style="padding-top:var(--wp--preset--spacing--50);padding-bottom:var(--wp--preset--spacing--50)"><!-- wp:image {"align":"center","id":14621,"sizeSlug":"full","linkDestination":"none"} -->
 <figure class="wp-block-image aligncenter size-full"><img src="https://wordpress.org/files/2022/12/NYP.png" alt="New York Post logo" class="wp-image-14621"/></figure>
 <!-- /wp:image -->
 
@@ -76,6 +74,7 @@
 <figure class="wp-block-image aligncenter size-full"><img src="https://wordpress.org/files/2022/12/CNN.png" alt="CNN logo" class="wp-image-14631"/></figure>
 <!-- /wp:image --></div>
 <!-- /wp:group --></div>
+<!-- /wp:group --></div>
 <!-- /wp:group -->
 
 <!-- wp:group {"align":"full","layout":{"type":"constrained"}} -->
@@ -87,8 +86,8 @@
 <!-- /wp:column -->
 
 <!-- wp:column {"width":"63%","style":{"spacing":{"padding":{"left":"var:preset|spacing|60","right":"0","top":"0","bottom":"0"}}}} -->
-<div class="wp-block-column" style="padding-top:0;padding-right:0;padding-bottom:0;padding-left:var(--wp--preset--spacing--60);flex-basis:63%"><!-- wp:heading {"level":3,"style":{"spacing":{"margin":{"top":"0","right":"0","bottom":"0","left":"0"}}},"className":"wp-block-heading"} -->
-<h3 class="wp-block-heading" style="margin-top:0;margin-right:0;margin-bottom:0;margin-left:0">Enterprise use cases</h3>
+<div class="wp-block-column" style="padding-top:0;padding-right:0;padding-bottom:0;padding-left:var(--wp--preset--spacing--60);flex-basis:63%"><!-- wp:heading {"style":{"spacing":{"margin":{"top":"0","right":"0","bottom":"0","left":"0"}}},"className":"wp-block-heading","fontSize":"heading-3"} -->
+<h2 class="wp-block-heading has-heading-3-font-size" style="margin-top:0;margin-right:0;margin-bottom:0;margin-left:0">Enterprise use cases</h2>
 <!-- /wp:heading -->
 
 <!-- wp:paragraph {"style":{"spacing":{"padding":{"bottom":"var:preset|spacing|60"}}}} -->
@@ -97,8 +96,8 @@
 
 <!-- wp:columns {"style":{"spacing":{"padding":{"bottom":"0"},"margin":{"top":"0","bottom":"0"},"blockGap":{"top":"0","left":"0"}}}} -->
 <div class="wp-block-columns" style="margin-top:0;margin-bottom:0;padding-bottom:0"><!-- wp:column {"style":{"spacing":{"padding":{"right":"var:preset|spacing|60","bottom":"var:preset|spacing|50"},"blockGap":"0"}}} -->
-<div class="wp-block-column" style="padding-right:var(--wp--preset--spacing--60);padding-bottom:var(--wp--preset--spacing--50)"><!-- wp:heading {"style":{"typography":{"fontStyle":"normal","fontWeight":"600"}},"className":"wp-block-heading is-style-default","fontSize":"large","fontFamily":"inter"} -->
-<h2 class="wp-block-heading is-style-default has-inter-font-family has-large-font-size" style="font-style:normal;font-weight:600">Media and publishing</h2>
+<div class="wp-block-column" style="padding-right:var(--wp--preset--spacing--60);padding-bottom:var(--wp--preset--spacing--50)"><!-- wp:heading {"level":3,"style":{"typography":{"fontStyle":"normal","fontWeight":"600"}},"className":"wp-block-heading is-style-default","fontSize":"large","fontFamily":"inter"} -->
+<h3 class="wp-block-heading is-style-default has-inter-font-family has-large-font-size" style="font-style:normal;font-weight:600">Media and publishing</h3>
 <!-- /wp:heading -->
 
 <!-- wp:paragraph {"style":{"spacing":{"padding":{"top":"var:preset|spacing|10"}}},"fontSize":"small"} -->
@@ -107,8 +106,8 @@
 <!-- /wp:column -->
 
 <!-- wp:column {"style":{"spacing":{"padding":{"top":"0","right":"0","bottom":"var:preset|spacing|50","left":"0"},"blockGap":"0"}}} -->
-<div class="wp-block-column" style="padding-top:0;padding-right:0;padding-bottom:var(--wp--preset--spacing--50);padding-left:0"><!-- wp:heading {"style":{"typography":{"fontStyle":"normal","fontWeight":"600"}},"className":"wp-block-heading","fontSize":"large","fontFamily":"inter"} -->
-<h2 class="wp-block-heading has-inter-font-family has-large-font-size" style="font-style:normal;font-weight:600">E-commerce</h2>
+<div class="wp-block-column" style="padding-top:0;padding-right:0;padding-bottom:var(--wp--preset--spacing--50);padding-left:0"><!-- wp:heading {"level":3,"style":{"typography":{"fontStyle":"normal","fontWeight":"600"}},"className":"wp-block-heading","fontSize":"large","fontFamily":"inter"} -->
+<h3 class="wp-block-heading has-inter-font-family has-large-font-size" style="font-style:normal;font-weight:600">E-commerce</h3>
 <!-- /wp:heading -->
 
 <!-- wp:paragraph {"style":{"spacing":{"padding":{"top":"var:preset|spacing|10"}}},"fontSize":"small"} -->
@@ -119,8 +118,8 @@
 
 <!-- wp:columns {"style":{"spacing":{"padding":{"bottom":"var:preset|spacing|40","top":"0"},"margin":{"top":"0","bottom":"0"},"blockGap":{"top":"0","left":"0"}}}} -->
 <div class="wp-block-columns" style="margin-top:0;margin-bottom:0;padding-top:0;padding-bottom:var(--wp--preset--spacing--40)"><!-- wp:column {"style":{"spacing":{"padding":{"right":"var:preset|spacing|60","top":"0","bottom":"var:preset|spacing|40"}}}} -->
-<div class="wp-block-column" style="padding-top:0;padding-right:var(--wp--preset--spacing--60);padding-bottom:var(--wp--preset--spacing--40)"><!-- wp:heading {"style":{"typography":{"fontStyle":"normal","fontWeight":"600"},"spacing":{"margin":{"top":"0","right":"0","bottom":"0","left":"0"},"padding":{"top":"0","right":"0","bottom":"0","left":"0"}}},"className":"wp-block-heading","fontSize":"large","fontFamily":"inter"} -->
-<h2 class="wp-block-heading has-inter-font-family has-large-font-size" style="margin-top:0;margin-right:0;margin-bottom:0;margin-left:0;padding-top:0;padding-right:0;padding-bottom:0;padding-left:0;font-style:normal;font-weight:600">Content marketing</h2>
+<div class="wp-block-column" style="padding-top:0;padding-right:var(--wp--preset--spacing--60);padding-bottom:var(--wp--preset--spacing--40)"><!-- wp:heading {"level":3,"style":{"typography":{"fontStyle":"normal","fontWeight":"600"},"spacing":{"margin":{"top":"0","right":"0","bottom":"0","left":"0"},"padding":{"top":"0","right":"0","bottom":"0","left":"0"}}},"className":"wp-block-heading","fontSize":"large","fontFamily":"inter"} -->
+<h3 class="wp-block-heading has-inter-font-family has-large-font-size" style="margin-top:0;margin-right:0;margin-bottom:0;margin-left:0;padding-top:0;padding-right:0;padding-bottom:0;padding-left:0;font-style:normal;font-weight:600">Content marketing</h3>
 <!-- /wp:heading -->
 
 <!-- wp:paragraph {"fontSize":"small"} -->
@@ -129,8 +128,8 @@
 <!-- /wp:column -->
 
 <!-- wp:column {"style":{"spacing":{"blockGap":"0","padding":{"bottom":"var:preset|spacing|40"}}}} -->
-<div class="wp-block-column" style="padding-bottom:var(--wp--preset--spacing--40)"><!-- wp:heading {"style":{"typography":{"fontStyle":"normal","fontWeight":"600"}},"className":"wp-block-heading","fontSize":"large","fontFamily":"inter"} -->
-<h2 class="wp-block-heading has-inter-font-family has-large-font-size" style="font-style:normal;font-weight:600">Higher education</h2>
+<div class="wp-block-column" style="padding-bottom:var(--wp--preset--spacing--40)"><!-- wp:heading {"level":3,"style":{"typography":{"fontStyle":"normal","fontWeight":"600"}},"className":"wp-block-heading","fontSize":"large","fontFamily":"inter"} -->
+<h3 class="wp-block-heading has-inter-font-family has-large-font-size" style="font-style:normal;font-weight:600">Higher education</h3>
 <!-- /wp:heading -->
 
 <!-- wp:paragraph {"style":{"spacing":{"padding":{"top":"var:preset|spacing|10","right":"0","bottom":"0","left":"0"}}},"fontSize":"small"} -->
@@ -149,8 +148,8 @@
 <figure class="wp-block-image size-full is-resized"><img src="https://wordpress.org/files/2022/11/icon-extensibility.png" alt="Extensibility icon" class="wp-image-13887" width="55"/></figure>
 <!-- /wp:image -->
 
-<!-- wp:heading {"level":3,"className":"wp-block-heading"} -->
-<h3 class="wp-block-heading">Extensibility</h3>
+<!-- wp:heading {"level":4,"className":"wp-block-heading"} -->
+<h4 class="wp-block-heading">Extensibility</h4>
 <!-- /wp:heading -->
 
 <!-- wp:paragraph -->
@@ -163,8 +162,8 @@
 <figure class="wp-block-image size-full is-resized"><img src="https://wordpress.org/files/2022/11/icon-security.png" alt="Security icon" class="wp-image-13886" width="47"/></figure>
 <!-- /wp:image -->
 
-<!-- wp:heading {"level":3,"className":"wp-block-heading"} -->
-<h3 class="wp-block-heading">Security</h3>
+<!-- wp:heading {"level":4,"className":"wp-block-heading"} -->
+<h4 class="wp-block-heading">Security</h4>
 <!-- /wp:heading -->
 
 <!-- wp:paragraph -->
@@ -177,8 +176,8 @@
 <figure class="wp-block-image size-full is-resized"><img src="https://wordpress.org/files/2022/11/icon-open-source.png" alt="Open Source Icon" class="wp-image-13885" width="62"/></figure>
 <!-- /wp:image -->
 
-<!-- wp:heading {"level":3,"className":"wp-block-heading"} -->
-<h3 class="wp-block-heading">Open source</h3>
+<!-- wp:heading {"level":4,"className":"wp-block-heading"} -->
+<h4 class="wp-block-heading">Open source</h4>
 <!-- /wp:heading -->
 
 <!-- wp:paragraph -->
@@ -188,14 +187,14 @@
 <!-- /wp:columns --></div>
 <!-- /wp:group -->
 
-<!-- wp:group {"align":"full","style":{"elements":{"link":{"color":{"text":"var:preset|color|blueberry-2"}}},"spacing":{"padding":{"top":"var:preset|spacing|60","right":"var:preset|spacing|60","bottom":"var:preset|spacing|60","left":"var:preset|spacing|60"}},"border":{"top":{"color":"var:preset|color|light-grey-1","width":"1px"},"right":{"width":"0px","style":"none"},"bottom":{"width":"0px","style":"none"},"left":{"width":"0px","style":"none"}}},"backgroundColor":"light-grey-2","textColor":"charcoal-1","layout":{"inherit":true,"type":"constrained","justifyContent":"center"}} -->
-<div class="wp-block-group alignfull has-charcoal-1-color has-light-grey-2-background-color has-text-color has-background has-link-color" style="border-top-color:var(--wp--preset--color--light-grey-1);border-top-width:1px;border-right-style:none;border-right-width:0px;border-bottom-style:none;border-bottom-width:0px;border-left-style:none;border-left-width:0px;padding-top:var(--wp--preset--spacing--60);padding-right:var(--wp--preset--spacing--60);padding-bottom:var(--wp--preset--spacing--60);padding-left:var(--wp--preset--spacing--60)"><!-- wp:group {"align":"wide","style":{"elements":{"link":{"color":{"text":"var:preset|color|blueberry-1"}}},"spacing":{"padding":{"top":"0","right":"0","bottom":"0","left":"0"}}}} -->
-<div class="wp-block-group alignwide has-link-color" style="padding-top:0;padding-right:0;padding-bottom:0;padding-left:0"><!-- wp:heading {"align":"wide","style":{"typography":{"fontStyle":"normal","fontWeight":"300","letterSpacing":"-2px"}},"className":"wp-block-heading is-style-with-arrow","fontSize":"heading-cta","fontFamily":"inter"} -->
-<h2 class="alignwide wp-block-heading is-style-with-arrow has-inter-font-family has-heading-cta-font-size" style="font-style:normal;font-weight:300;letter-spacing:-2px"><a href="https://wordpress.org/download/">Try WordPress</a></h2>
+<!-- wp:group {"align":"full","style":{"border":{"top":{"color":"var:preset|color|light-grey-1","width":"1px"},"right":{"width":"0px","style":"none"},"bottom":{"width":"0px","style":"none"},"left":{"width":"0px","style":"none"}}},"backgroundColor":"light-grey-2","textColor":"charcoal-1","layout":{"inherit":true,"type":"constrained","justifyContent":"center"}} -->
+<div class="wp-block-group alignfull has-charcoal-1-color has-light-grey-2-background-color has-text-color has-background" style="border-top-color:var(--wp--preset--color--light-grey-1);border-top-width:1px;border-right-style:none;border-right-width:0px;border-bottom-style:none;border-bottom-width:0px;border-left-style:none;border-left-width:0px"><!-- wp:group {"align":"full","style":{"spacing":{"padding":{"top":"var:preset|spacing|60","right":"var:preset|spacing|60","bottom":"var:preset|spacing|60","left":"var:preset|spacing|60"}}},"layout":{"type":"constrained"}} -->
+<div class="wp-block-group alignfull" style="padding-top:var(--wp--preset--spacing--60);padding-right:var(--wp--preset--spacing--60);padding-bottom:var(--wp--preset--spacing--60);padding-left:var(--wp--preset--spacing--60)"><!-- wp:heading {"align":"full","style":{"typography":{"fontStyle":"normal","fontWeight":"300","letterSpacing":"-2px"}},"className":"wp-block-heading is-style-with-arrow","fontSize":"heading-cta","fontFamily":"inter"} -->
+<h2 class="wp-block-heading alignfull is-style-with-arrow has-inter-font-family has-heading-cta-font-size" style="font-style:normal;font-weight:300;letter-spacing:-2px"><a href="https://wordpress.org/download/">Try WordPress</a></h2>
 <!-- /wp:heading -->
 
-<!-- wp:columns {"style":{"border":{"radius":"0px"},"spacing":{"padding":{"top":"var:preset|spacing|70"}}}} -->
-<div class="wp-block-columns" style="border-radius:0px;padding-top:var(--wp--preset--spacing--70)"><!-- wp:column {"className":"wp-block-navigation"} -->
+<!-- wp:columns {"align":"full","style":{"border":{"radius":"0px"},"spacing":{"padding":{"top":"var:preset|spacing|70"}},"elements":{"link":{"color":{"text":"var:preset|color|blueberry-1"}}}}} -->
+<div class="wp-block-columns alignfull has-link-color" style="border-radius:0px;padding-top:var(--wp--preset--spacing--70)"><!-- wp:column {"className":"wp-block-navigation"} -->
 <div class="wp-block-column wp-block-navigation"><!-- wp:heading {"level":5,"className":"wp-block-heading"} -->
 <h5 class="wp-block-heading">Case studies</h5>
 <!-- /wp:heading -->
@@ -246,7 +245,7 @@
 <!-- /wp:list-item -->
 
 <!-- wp:list-item -->
-<li><a href="https://vimeo.com/374961815/040fe06be7">Gutenberg for Enterprise</a>↗</li>
+<li><a href="https://vimeo.com/374961815/040fe06be7">Gutenberg for Enterprise↗</a></li>
 <!-- /wp:list-item -->
 
 <!-- wp:list-item -->
