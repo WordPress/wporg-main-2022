b1868603fd7854740bece25707c166858471b653
Kelly Dwan <ryelle@users.noreply.github.com>
2023-01-02 12:38:30 -0500

    Patterns: Update release page content

--- source/wp-content/themes/wporg-main-2022/patterns/download-releases.php
+++ source/wp-content/themes/wporg-main-2022/patterns/download-releases.php
@@ -26,7 +26,7 @@
 
 <!-- wp:column {"width":"66.66%"} -->
 <div class="wp-block-column" style="flex-basis:66.66%"><!-- wp:paragraph -->
-<p><?php _e( 'This is an archive of every release we’ve done that we have a record of.<br>None of these are safe to use, except the <strong>latest</strong> in the 6.0 series, which is actively maintained.', 'wporg' ); ?></p>
+<p><?php _e( 'This is an archive of every release we’ve done that we have a record of.<br>None of these are safe to use, except the <strong>latest</strong> in the 6.1 series, which is actively maintained.', 'wporg' ); ?></p>
 <!-- /wp:paragraph -->
 
 <!-- wp:paragraph -->
