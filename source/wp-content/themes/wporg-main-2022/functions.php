affa0a5e8645ced62d5274b39e62da9906a5f840
Kelly Dwan <ryelle@users.noreply.github.com>
2023-01-11 11:35:23 -0500

    Try new design for search results page (#159)
    * Add a new block for rendering the Google Search embed
    
    * Create a page template for the search page
    
    * Enable the new theme on search result pages
    
    * If jQuery is not enqueued, add it.

--- source/wp-content/themes/wporg-main-2022/functions.php
+++ source/wp-content/themes/wporg-main-2022/functions.php
@@ -10,6 +10,7 @@ require_once __DIR__ . '/inc/capabilities.php';
 
 // Block files
 require_once __DIR__ . '/src/download-counter/index.php';
+require_once __DIR__ . '/src/google-search-embed/index.php';
 require_once __DIR__ . '/src/random-heading/index.php';
 require_once __DIR__ . '/src/release-tables/index.php';
 
