8dff1b3c5d806a952b545fac9a9c43536b9654da
Adam Wood <adam.wood@automattic.com>
2023-01-23 16:23:13 +1300

    Add skeleton pages with nav and h1

---
+++ source/wp-content/themes/wporg-main-2022/patterns/security.php
@@ -0,0 +1,25 @@
+<?php
+/**
+ * Title: Security 2
+ * Slug: wporg-main-2022/security-2
+ * Inserter: no
+ */
+
+?>
+<!-- wp:wporg/global-header {"style":"black-on-white"} /-->
+
+<!-- wp:group {"align":"full","style":{"spacing":{"padding":{"top":"var:preset|spacing|20","right":"var:preset|spacing|edge-space","bottom":"var:preset|spacing|20","left":"var:preset|spacing|edge-space"}}},"layout":{"type":"constrained"}} -->
+<div class="wp-block-group alignfull" style="padding-top:var(--wp--preset--spacing--20);padding-right:var(--wp--preset--spacing--edge-space);padding-bottom:var(--wp--preset--spacing--20);padding-left:var(--wp--preset--spacing--edge-space)"><!-- wp:group {"align":"full","layout":{"type":"flex","flexWrap":"nowrap","justifyContent":"space-between"}} -->
+<div class="wp-block-group alignfull"><!-- wp:paragraph {"style":{"typography":{"fontStyle":"normal","fontWeight":"700","lineHeight":1.9}},"fontSize":"small"} -->
+<p class="has-small-font-size" style="font-style:normal;font-weight:700;line-height:1.9"><?php _e( 'About', 'wporg' ); ?></p>
+<!-- /wp:paragraph -->
+
+<!-- wp:navigation {"ref":16421,"textColor":"blueberry-1","align":"wide","className":"is-style-default","layout":{"type":"flex","justifyContent":"right"},"fontSize":"small"} /--></div>
+<!-- /wp:group --></div>
+<!-- /wp:group -->
+
+<!-- wp:heading {"level":1,"fontSize":"heading-2"} -->
+<h1 class="wp-block-heading has-heading-2-font-size"><?php _e( 'Security', 'wporg' ); ?></h1>
+<!-- /wp:heading -->
+
+<!-- wp:wporg/global-footer {"style":"black-on-white"} /-->
