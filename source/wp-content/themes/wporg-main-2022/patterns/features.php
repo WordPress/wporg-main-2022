eb63ed25e6c9d84485cd0c72547cda598c8e0dff
Adam Wood <adam.wood@automattic.com>
2023-02-01 16:44:02 +1300

    Make sub nav sticky

--- source/wp-content/themes/wporg-main-2022/patterns/features.php
+++ source/wp-content/themes/wporg-main-2022/patterns/features.php
@@ -6,13 +6,13 @@
  */
 
 ?>
-<!-- wp:group {"align":"full","style":{"spacing":{"padding":{"top":"var:preset|spacing|20","right":"var:preset|spacing|edge-space","bottom":"var:preset|spacing|20","left":"var:preset|spacing|edge-space"}}},"layout":{"type":"constrained"}} -->
-<div class="wp-block-group alignfull" style="padding-top:var(--wp--preset--spacing--20);padding-right:var(--wp--preset--spacing--edge-space);padding-bottom:var(--wp--preset--spacing--20);padding-left:var(--wp--preset--spacing--edge-space)"><!-- wp:group {"align":"full","layout":{"type":"flex","flexWrap":"nowrap","justifyContent":"space-between"}} -->
+<!-- wp:group {"align":"full","style":{"spacing":{"padding":{"top":"var:preset|spacing|20","right":"var:preset|spacing|edge-space","bottom":"var:preset|spacing|20","left":"var:preset|spacing|edge-space"}}},"backgroundColor":"white","className":"is-sticky","layout":{"type":"constrained"}} -->
+<div class="wp-block-group alignfull is-sticky has-white-background-color has-background" style="padding-top:var(--wp--preset--spacing--20);padding-right:var(--wp--preset--spacing--edge-space);padding-bottom:var(--wp--preset--spacing--20);padding-left:var(--wp--preset--spacing--edge-space)"><!-- wp:group {"align":"full","layout":{"type":"flex","flexWrap":"nowrap","justifyContent":"space-between"}} -->
 <div class="wp-block-group alignfull"><!-- wp:paragraph {"style":{"typography":{"fontStyle":"normal","fontWeight":"700","lineHeight":1.9}},"fontSize":"small"} -->
 <p class="has-small-font-size" style="font-style:normal;font-weight:700;line-height:1.9"><?php _e( 'About', 'wporg' ); ?></p>
 <!-- /wp:paragraph -->
 
-<!-- wp:navigation {"ref":16421,"textColor":"blueberry-1","align":"wide","className":"is-style-default","layout":{"type":"flex","justifyContent":"right"},"fontSize":"small"} /--></div>
+<!-- wp:navigation {"ref":3,"textColor":"blueberry-1","align":"wide","layout":{"type":"flex","justifyContent":"right"},"fontSize":"small"} /--></div>
 <!-- /wp:group --></div>
 <!-- /wp:group -->
 
