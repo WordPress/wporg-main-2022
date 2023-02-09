ee5043bcdc75620005974867fde44272cafefdab
Adam Wood <adam.wood@automattic.com>
2023-02-09 15:01:38 +1300

    Add Logos page content

--- source/wp-content/themes/wporg-main-2022/patterns/logos.php
+++ source/wp-content/themes/wporg-main-2022/patterns/logos.php
@@ -12,12 +12,184 @@
 <p class="has-small-font-size" style="font-style:normal;font-weight:700"><?php _e( 'About', 'wporg' ); ?></p>
 <!-- /wp:paragraph -->
 
-<!-- wp:navigation {"ref":16920,"textColor":"blueberry-1","backgroundColor":"white","align":"wide","className":"is-style-dropdown-on-mobile","layout":{"type":"flex","justifyContent":"left"},"style":{"spacing":{"blockGap":"24px"}},"fontSize":"small"} /--></div>
+<!-- wp:navigation {"ref":17028,"textColor":"blueberry-1","backgroundColor":"white","align":"wide","className":"is-style-dropdown-on-mobile","layout":{"type":"flex","justifyContent":"left"},"style":{"spacing":{"blockGap":"24px"}},"fontSize":"small"} /--></div>
 <!-- /wp:group --></div>
 <!-- /wp:group -->
 
 <!-- wp:group {"align":"full","style":{"spacing":{"padding":{"top":"var:preset|spacing|60","right":"var:preset|spacing|edge-space","left":"var:preset|spacing|edge-space","bottom":"var:preset|spacing|60"}}},"layout":{"type":"constrained"}} -->
 <div class="wp-block-group alignfull" style="padding-top:var(--wp--preset--spacing--60);padding-right:var(--wp--preset--spacing--edge-space);padding-bottom:var(--wp--preset--spacing--60);padding-left:var(--wp--preset--spacing--edge-space)"><!-- wp:heading {"level":1,"style":{"spacing":{"margin":{"bottom":"var:preset|spacing|30"}}},"fontSize":"heading-2"} -->
 <h1 class="wp-block-heading has-heading-2-font-size" style="margin-bottom:var(--wp--preset--spacing--30)"><?php _e( 'Graphics &amp; Logos', 'wporg' ); ?></h1>
-<!-- /wp:heading --></div>
+<!-- /wp:heading -->
+
+<!-- wp:heading -->
+<h2 class="wp-block-heading"><?php _e( 'Official WordPress Logo', 'wporg' ); ?></h2>
+<!-- /wp:heading -->
+
+<!-- wp:paragraph -->
+<p><?php _e( 'When you need the official WordPress logo for a web site or publication, please use one of the following. Please only use logos in accordance with the <a href="http://wordpressfoundation.org/trademark-policy/">WordPress trademark policy</a>.', 'wporg' ); ?></p>
+<!-- /wp:paragraph -->
+
+<!-- wp:heading -->
+<h2 class="wp-block-heading"><?php _e( 'Downloads', 'wporg' ); ?></h2>
+<!-- /wp:heading -->
+
+<!-- wp:columns -->
+<div class="wp-block-columns"><!-- wp:column -->
+<div class="wp-block-column"><!-- wp:image {"id":17151,"sizeSlug":"full","linkDestination":"media"} -->
+<figure class="wp-block-image size-full"><a href="https://wordpress.org/files/2023/02/allversions.png"><img src="https://wordpress.org/files/2023/02/allversions.png" alt="" class="wp-image-17151" /></a></figure>
+<!-- /wp:image -->
+
+<!-- wp:heading {"level":3,"fontSize":"large"} -->
+<h3 class="wp-block-heading has-large-font-size"><?php _e( '<strong>WordPress Logotypes</strong>', 'wporg' ); ?></h3>
+<!-- /wp:heading -->
+
+<!-- wp:paragraph {"className":"is-style-short-text","fontSize":"small"} -->
+<p class="is-style-short-text has-small-font-size"><?php _e( 'All versions', 'wporg' ); ?></p>
+<!-- /wp:paragraph -->
+
+<!-- wp:paragraph {"className":"is-style-short-text","fontSize":"small"} -->
+<p class="is-style-short-text has-small-font-size"><?php _e( '<a href="https://s.w.org/style/images/about/WordPress-logotype-all.ai_.zip">Adobe Illustrator CS5+ Sketch 3</a>', 'wporg' ); ?></p>
+<!-- /wp:paragraph --></div>
+<!-- /wp:column -->
+
+<!-- wp:column -->
+<div class="wp-block-column"><!-- wp:image {"id":17160,"sizeSlug":"full","linkDestination":"media"} -->
+<figure class="wp-block-image size-full"><a href="https://wordpress.org/files/2023/02/standard.png"><img src="https://wordpress.org/files/2023/02/standard.png" alt="" class="wp-image-17160" /></a></figure>
+<!-- /wp:image -->
+
+<!-- wp:heading {"level":3,"fontSize":"large"} -->
+<h3 class="wp-block-heading has-large-font-size"><?php _e( '<strong>WordPress Logotype</strong>', 'wporg' ); ?></h3>
+<!-- /wp:heading -->
+
+<!-- wp:paragraph {"className":"is-style-short-text","fontSize":"small"} -->
+<p class="is-style-short-text has-small-font-size"><?php _e( 'Standard', 'wporg' ); ?></p>
+<!-- /wp:paragraph -->
+
+<!-- wp:paragraph {"className":"is-style-short-text","fontSize":"small"} -->
+<p class="is-style-short-text has-small-font-size"><?php _e( '<a href="https://s.w.org/style/images/about/WordPress-logotype-standard.pdf">PDF&nbsp;(Vector)</a>', 'wporg' ); ?></p>
+<!-- /wp:paragraph -->
+
+<!-- wp:paragraph {"className":"is-style-short-text","fontSize":"small"} -->
+<p class="is-style-short-text has-small-font-size"><?php _e( '<a href="https://s.w.org/style/images/about/WordPress-logotype-standard.png">PNG&nbsp;(BaseGray/transparent)</a>', 'wporg' ); ?></p>
+<!-- /wp:paragraph -->
+
+<!-- wp:paragraph {"className":"is-style-short-text","fontSize":"small"} -->
+<p class="is-style-short-text has-small-font-size"><?php _e( '<a href="https://s.w.org/style/images/about/WordPress-logotype-standard-white.png">PNG&nbsp;(White/transparent)</a>', 'wporg' ); ?></p>
+<!-- /wp:paragraph --></div>
+<!-- /wp:column -->
+
+<!-- wp:column -->
+<div class="wp-block-column"><!-- wp:image {"id":17161,"sizeSlug":"full","linkDestination":"media"} -->
+<figure class="wp-block-image size-full"><a href="https://wordpress.org/files/2023/02/alternative.png"><img src="https://wordpress.org/files/2023/02/alternative.png" alt="" class="wp-image-17161" /></a></figure>
+<!-- /wp:image -->
+
+<!-- wp:heading {"level":3,"fontSize":"large"} -->
+<h3 class="wp-block-heading has-large-font-size"><?php _e( '<strong>WordPress Logotype</strong>', 'wporg' ); ?></h3>
+<!-- /wp:heading -->
+
+<!-- wp:paragraph {"className":"is-style-short-text","fontSize":"small"} -->
+<p class="is-style-short-text has-small-font-size"><?php _e( 'Alternative, vertical arrangement', 'wporg' ); ?></p>
+<!-- /wp:paragraph -->
+
+<!-- wp:paragraph {"className":"is-style-short-text","fontSize":"small"} -->
+<p class="is-style-short-text has-small-font-size"><?php _e( '<a href="https://s.w.org/style/images/about/WordPress-logotype-alternative.pdf">PDF&nbsp;(Vector)</a>', 'wporg' ); ?></p>
+<!-- /wp:paragraph -->
+
+<!-- wp:paragraph {"className":"is-style-short-text","fontSize":"small"} -->
+<p class="is-style-short-text has-small-font-size"><?php _e( '<a href="https://s.w.org/style/images/about/WordPress-logotype-alternative.png">PNG&nbsp;(BaseGray/transparent)</a>', 'wporg' ); ?></p>
+<!-- /wp:paragraph -->
+
+<!-- wp:paragraph {"className":"is-style-short-text","fontSize":"small"} -->
+<p class="is-style-short-text has-small-font-size"><?php _e( '<a href="https://s.w.org/style/images/about/WordPress-logotype-alternative-white.png">PNG&nbsp;(White/transparent)</a>', 'wporg' ); ?></p>
+<!-- /wp:paragraph --></div>
+<!-- /wp:column --></div>
+<!-- /wp:columns -->
+
+<!-- wp:columns -->
+<div class="wp-block-columns"><!-- wp:column -->
+<div class="wp-block-column"><!-- wp:image {"id":17166,"sizeSlug":"full","linkDestination":"media"} -->
+<figure class="wp-block-image size-full"><a href="https://wordpress.org/files/2023/02/wordmark.png"><img src="https://wordpress.org/files/2023/02/wordmark.png" alt="" class="wp-image-17166" /></a></figure>
+<!-- /wp:image -->
+
+<!-- wp:heading {"level":3,"fontSize":"large"} -->
+<h3 class="wp-block-heading has-large-font-size"><?php _e( '<strong>WordPress Logotypes</strong>', 'wporg' ); ?></h3>
+<!-- /wp:heading -->
+
+<!-- wp:paragraph {"className":"is-style-short-text","fontSize":"small"} -->
+<p class="is-style-short-text has-small-font-size"><?php _e( 'Word Mark', 'wporg' ); ?></p>
+<!-- /wp:paragraph -->
+
+<!-- wp:paragraph {"className":"is-style-short-text","fontSize":"small"} -->
+<p class="is-style-short-text has-small-font-size"><?php _e( '<a href="https://s.w.org/style/images/about/WordPress-logotype-wordmark.pdf">PDF&nbsp;(Vector)</a>', 'wporg' ); ?></p>
+<!-- /wp:paragraph -->
+
+<!-- wp:paragraph {"className":"is-style-short-text","fontSize":"small"} -->
+<p class="is-style-short-text has-small-font-size"><?php _e( '<a href="https://s.w.org/style/images/about/WordPress-logotype-wordmark.png">PNG&nbsp;(BaseGray/transparent)</a>', 'wporg' ); ?></p>
+<!-- /wp:paragraph -->
+
+<!-- wp:paragraph {"className":"is-style-short-text","fontSize":"small"} -->
+<p class="is-style-short-text has-small-font-size"><?php _e( '<a href="https://s.w.org/style/images/about/WordPress-logotype-wordmark-white.png">PNG&nbsp;(White/transparent)</a>', 'wporg' ); ?></p>
+<!-- /wp:paragraph --></div>
+<!-- /wp:column -->
+
+<!-- wp:column -->
+<div class="wp-block-column"><!-- wp:image {"id":17167,"sizeSlug":"full","linkDestination":"media"} -->
+<figure class="wp-block-image size-full"><a href="https://wordpress.org/files/2023/02/wmark.png"><img src="https://wordpress.org/files/2023/02/wmark.png" alt="" class="wp-image-17167" /></a></figure>
+<!-- /wp:image -->
+
+<!-- wp:heading {"level":3,"fontSize":"large"} -->
+<h3 class="wp-block-heading has-large-font-size"><?php _e( '<strong>WordPress Logotype</strong>', 'wporg' ); ?></h3>
+<!-- /wp:heading -->
+
+<!-- wp:paragraph {"className":"is-style-short-text","fontSize":"small"} -->
+<p class="is-style-short-text has-small-font-size"><?php _e( 'W Mark', 'wporg' ); ?></p>
+<!-- /wp:paragraph -->
+
+<!-- wp:paragraph {"className":"is-style-short-text","fontSize":"small"} -->
+<p class="is-style-short-text has-small-font-size"><?php _e( '<a href="https://s.w.org/style/images/about/WordPress-logotype-wmark.pdf">PDF&nbsp;(Vector)</a>', 'wporg' ); ?></p>
+<!-- /wp:paragraph -->
+
+<!-- wp:paragraph {"className":"is-style-short-text","fontSize":"small"} -->
+<p class="is-style-short-text has-small-font-size"><?php _e( '<a href="https://s.w.org/style/images/about/WordPress-logotype-wmark.png">PNG&nbsp;(BaseGray/transparent)</a>', 'wporg' ); ?></p>
+<!-- /wp:paragraph -->
+
+<!-- wp:paragraph {"className":"is-style-short-text","fontSize":"small"} -->
+<p class="is-style-short-text has-small-font-size"><?php _e( '<a href="https://s.w.org/style/images/about/WordPress-logotype-wmark-white.png">PNG&nbsp;(White/transparent)</a>', 'wporg' ); ?></p>
+<!-- /wp:paragraph --></div>
+<!-- /wp:column -->
+
+<!-- wp:column -->
+<div class="wp-block-column"><!-- wp:image {"id":17168,"sizeSlug":"full","linkDestination":"media"} -->
+<figure class="wp-block-image size-full"><a href="https://wordpress.org/files/2023/02/simplified.png"><img src="https://wordpress.org/files/2023/02/simplified.png" alt="" class="wp-image-17168" /></a></figure>
+<!-- /wp:image -->
+
+<!-- wp:heading {"level":3,"fontSize":"large"} -->
+<h3 class="wp-block-heading has-large-font-size"><?php _e( '<strong>WordPress Logotype</strong>', 'wporg' ); ?></h3>
+<!-- /wp:heading -->
+
+<!-- wp:paragraph {"className":"is-style-short-text","fontSize":"small"} -->
+<p class="is-style-short-text has-small-font-size"><?php _e( 'Simplified', 'wporg' ); ?></p>
+<!-- /wp:paragraph -->
+
+<!-- wp:paragraph {"className":"is-style-short-text","fontSize":"small"} -->
+<p class="is-style-short-text has-small-font-size"><?php _e( '<a href="https://s.w.org/style/images/about/WordPress-logotype-simplified.pdf?1">PDF&nbsp;(Vector)</a>', 'wporg' ); ?></p>
+<!-- /wp:paragraph -->
+
+<!-- wp:paragraph {"className":"is-style-short-text","fontSize":"small"} -->
+<p class="is-style-short-text has-small-font-size"><?php _e( '<a href="https://s.w.org/style/images/about/WordPress-logotype-simplified.png">PNG&nbsp;(BaseGray/transparent)</a>', 'wporg' ); ?></p>
+<!-- /wp:paragraph --></div>
+<!-- /wp:column --></div>
+<!-- /wp:columns -->
+
+<!-- wp:heading -->
+<h2 class="wp-block-heading"><?php _e( 'Fight the Fake Logo (Fauxgo)', 'wporg' ); ?></h2>
+<!-- /wp:heading -->
+
+<!-- wp:paragraph -->
+<p><?php _e( 'Friends don’t let friends use the wrong WordPress logo. If you see one of these in the wild, please suggest a change.', 'wporg' ); ?></p>
+<!-- /wp:paragraph -->
+
+<!-- wp:image {"id":17154,"sizeSlug":"full","linkDestination":"media"} -->
+<figure class="wp-block-image size-full"><a href="https://wordpress.org/files/2023/02/logo-comparison.png"><img src="https://wordpress.org/files/2023/02/logo-comparison.png" alt="" class="wp-image-17154" /></a></figure>
+<!-- /wp:image --></div>
 <!-- /wp:group -->
