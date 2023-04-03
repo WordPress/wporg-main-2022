88409ceacc9767a1a9e433356a1100bef405ac1e
Kelly Dwan <ryelle@users.noreply.github.com>
2023-04-03 13:54:31 -0400

    Update content with new parser

--- source/wp-content/themes/wporg-main-2022/patterns/download.php
+++ source/wp-content/themes/wporg-main-2022/patterns/download.php
@@ -43,13 +43,13 @@
 <!-- /wp:paragraph -->
 
 <!-- wp:navigation {"textColor":"blueberry-1","overlayMenu":"never","className":"is-style-dots","style":{"spacing":{"blockGap":"0px"}},"fontSize":"small"} -->
-<!-- wp:navigation-link {"label":"Releases","url":"https://wordpress.org/download/releases/","kind":"custom","isTopLevelLink":true} /-->
+<!-- wp:navigation-link {"label":"<?php _e( 'Releases', 'wporg' ); ?>","url":"https://wordpress.org/download/releases/","kind":"custom","isTopLevelLink":true} /-->
 
-<!-- wp:navigation-link {"label":"Nightly","url":"https://wordpress.org/download/beta-nightly/","kind":"custom","isTopLevelLink":true} /-->
+<!-- wp:navigation-link {"label":"<?php _e( 'Nightly', 'wporg' ); ?>","url":"https://wordpress.org/download/beta-nightly/","kind":"custom","isTopLevelLink":true} /-->
 
-<!-- wp:navigation-link {"label":"Counter","url":"https://wordpress.org/download/counter/","kind":"custom","isTopLevelLink":true} /-->
+<!-- wp:navigation-link {"label":"<?php _e( 'Counter', 'wporg' ); ?>","url":"https://wordpress.org/download/counter/","kind":"custom","isTopLevelLink":true} /-->
 
-<!-- wp:navigation-link {"label":"Source","url":"https://wordpress.org/download/source/","kind":"custom","isTopLevelLink":true} /-->
+<!-- wp:navigation-link {"label":"<?php _e( 'Source', 'wporg' ); ?>","url":"https://wordpress.org/download/source/","kind":"custom","isTopLevelLink":true} /-->
 <!-- /wp:navigation --></div>
 <!-- /wp:group --></div>
 <!-- /wp:column -->
--- source/wp-content/themes/wporg-main-2022/patterns/features.php
+++ source/wp-content/themes/wporg-main-2022/patterns/features.php
@@ -6,8 +6,8 @@
  */
 
 ?>
-<!-- wp:group {"layout":{"type":"constrained"},"align":"full","style":{"spacing":{"padding":{"right":"var:preset|spacing|60","left":"var:preset|spacing|60","top":"16px","bottom":"16px"}}},"backgroundColor":"white","className":"is-sticky"} -->
-<div class="wp-block-group alignfull is-sticky has-white-background-color has-background" style="padding-top:16px;padding-right:var(--wp--preset--spacing--60);padding-bottom:16px;padding-left:var(--wp--preset--spacing--60)"><!-- wp:group {"layout":{"type":"flex","flexWrap":"wrap","justifyContent":"space-between"},"align":"full"} -->
+<!-- wp:group {"align":"full","style":{"spacing":{"padding":{"right":"var:preset|spacing|60","left":"var:preset|spacing|60","top":"16px","bottom":"16px"}}},"backgroundColor":"white","className":"is-sticky","layout":{"type":"constrained"}} -->
+<div class="wp-block-group alignfull is-sticky has-white-background-color has-background" style="padding-top:16px;padding-right:var(--wp--preset--spacing--60);padding-bottom:16px;padding-left:var(--wp--preset--spacing--60)"><!-- wp:group {"align":"full","layout":{"type":"flex","flexWrap":"wrap","justifyContent":"space-between"}} -->
 <div class="wp-block-group alignfull"><!-- wp:paragraph {"style":{"typography":{"fontStyle":"normal","fontWeight":"700"}},"fontSize":"small"} -->
 <p class="has-small-font-size" style="font-style:normal;font-weight:700"><?php _e( 'About', 'wporg' ); ?></p>
 <!-- /wp:paragraph -->
@@ -16,7 +16,7 @@
 <!-- /wp:group --></div>
 <!-- /wp:group -->
 
-<!-- wp:group {"layout":{"type":"constrained"},"align":"full","style":{"spacing":{"padding":{"top":"var:preset|spacing|60","right":"var:preset|spacing|edge-space","left":"var:preset|spacing|edge-space","bottom":"var:preset|spacing|60"}}}} -->
+<!-- wp:group {"align":"full","style":{"spacing":{"padding":{"top":"var:preset|spacing|60","right":"var:preset|spacing|edge-space","left":"var:preset|spacing|edge-space","bottom":"var:preset|spacing|60"}}},"layout":{"type":"constrained"}} -->
 <div class="wp-block-group alignfull" style="padding-top:var(--wp--preset--spacing--60);padding-right:var(--wp--preset--spacing--edge-space);padding-bottom:var(--wp--preset--spacing--60);padding-left:var(--wp--preset--spacing--edge-space)"><!-- wp:heading {"level":1,"style":{"spacing":{"margin":{"bottom":"var:preset|spacing|30"}}},"className":"wp-block-heading","fontSize":"heading-2"} -->
 <h1 class="wp-block-heading has-heading-2-font-size" style="margin-bottom:var(--wp--preset--spacing--30)"><?php _e( 'Features', 'wporg' ); ?></h1>
 <!-- /wp:heading -->
