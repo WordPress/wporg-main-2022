3a57b92a081a40dd8e3d432cef6c50d30a97fd67
Kelly Dwan <ryelle@users.noreply.github.com>
2023-05-09 16:50:36 -0400

    Patterns: Update mobile page layout
    Fixes #195

--- source/wp-content/themes/wporg-main-2022/patterns/mobile.php
+++ source/wp-content/themes/wporg-main-2022/patterns/mobile.php
@@ -6,19 +6,17 @@
  */
 
 ?>
-<!-- wp:group {"align":"full","style":{"spacing":{"padding":{"right":"var:preset|spacing|edge-space","left":"var:preset|spacing|edge-space"}}},"layout":{"type":"constrained"}} -->
-<div class="wp-block-group alignfull" style="padding-right:var(--wp--preset--spacing--edge-space);padding-left:var(--wp--preset--spacing--edge-space)"><!-- wp:cover {"dimRatio":0,"overlayColor":"white","minHeight":60,"minHeightUnit":"vh","isDark":false,"align":"wide","style":{"spacing":{"padding":{"top":"0","right":"0","bottom":"0","left":"0"}}}} -->
-<div class="wp-block-cover alignwide is-light" style="padding-top:0;padding-right:0;padding-bottom:0;padding-left:0;min-height:60vh"><span aria-hidden="true" class="wp-block-cover__background has-white-background-color has-background-dim-0 has-background-dim"></span><div class="wp-block-cover__inner-container"><!-- wp:columns {"verticalAlignment":"center","align":"wide","style":{"spacing":{"padding":{"top":"var:preset|spacing|60","bottom":"var:preset|spacing|60"}}}} -->
-<div class="wp-block-columns alignwide are-vertically-aligned-center" style="padding-top:var(--wp--preset--spacing--60);padding-bottom:var(--wp--preset--spacing--60)"><!-- wp:column {"verticalAlignment":"center","width":"50%","style":{"spacing":{"padding":{"right":"var:preset|spacing|50"}}}} -->
-<div class="wp-block-column is-vertically-aligned-center" style="padding-right:var(--wp--preset--spacing--50);flex-basis:50%"><!-- wp:heading {"level":1,"fontSize":"heading-3"} -->
-<h1 class="has-heading-3-font-size"><?php _e( 'WordPress mobile apps', 'wporg' ); ?></h1>
+<!-- wp:cover {"dimRatio":0,"overlayColor":"white","minHeight":60,"minHeightUnit":"vh","isDark":false,"align":"full","style":{"spacing":{"padding":{"top":"var:preset|spacing|60","right":"var:preset|spacing|edge-space","bottom":"var:preset|spacing|60","left":"var:preset|spacing|edge-space"}}},"layout":{"type":"constrained"}} -->
+<div class="wp-block-cover alignfull is-light" style="padding-top:var(--wp--preset--spacing--60);padding-right:var(--wp--preset--spacing--edge-space);padding-bottom:var(--wp--preset--spacing--60);padding-left:var(--wp--preset--spacing--edge-space);min-height:60vh"><span aria-hidden="true" class="wp-block-cover__background has-white-background-color has-background-dim-0 has-background-dim"></span><div class="wp-block-cover__inner-container"><!-- wp:group {"layout":{"type":"default"}} -->
+<div class="wp-block-group"><!-- wp:heading {"textAlign":"center","level":1,"fontSize":"heading-1"} -->
+<h1 class="wp-block-heading has-text-align-center has-heading-1-font-size"><?php _e( 'WordPress Mobile Apps', 'wporg' ); ?></h1>
 <!-- /wp:heading -->
 
-<!-- wp:paragraph -->
-<p><?php _e( 'Inspiration strikes any time, anywhere. WordPress mobile apps put the power of publishing in your hands. And of course, they’re open source, just like WordPress.', 'wporg' ); ?></p>
+<!-- wp:paragraph {"align":"center"} -->
+<p class="has-text-align-center"><?php _e( 'Inspiration strikes any time, anywhere. WordPress mobile apps put the power of publishing in your hands. And of course, they’re open source, just like WordPress.', 'wporg' ); ?></p>
 <!-- /wp:paragraph -->
 
-<!-- wp:group {"layout":{"type":"flex","flexWrap":"nowrap","justifyContent":"left"}} -->
+<!-- wp:group {"layout":{"type":"flex","flexWrap":"nowrap","justifyContent":"center"}} -->
 <div class="wp-block-group"><!-- wp:image {"width":150,"height":45,"linkDestination":"custom"} -->
 <figure class="wp-block-image is-resized"><a href="https://itunes.apple.com/app/apple-store/id335703880?pt=299112&amp;ct=wordpress.org&amp;mt=8"><img src="https://wordpress.org/wp-content/themes/pub/wporg-main/images/badge-apple.png" alt="<?php _e( 'Download on the Apple App Store', 'wporg' ); ?>" width="150" height="45" /></a></figure>
 <!-- /wp:image -->
@@ -28,16 +26,16 @@
 <!-- /wp:image --></div>
 <!-- /wp:group -->
 
-<!-- wp:paragraph {"textColor":"charcoal-4","className":"is-style-short-text","fontSize":"small"} -->
-<p class="is-style-short-text has-charcoal-4-color has-text-color has-small-font-size"><?php _e( 'Requires a hosted website with WordPress 4.0 or higher.', 'wporg' ); ?></p>
-<!-- /wp:paragraph --></div>
-<!-- /wp:column -->
+<!-- wp:paragraph {"align":"center","textColor":"charcoal-4","className":"is-style-short-text","fontSize":"small"} -->
+<p class="has-text-align-center is-style-short-text has-charcoal-4-color has-text-color has-small-font-size"><?php _e( 'Requires a hosted website with WordPress 4.0 or higher.', 'wporg' ); ?></p>
+<!-- /wp:paragraph -->
 
-<!-- wp:column {"verticalAlignment":"center","width":"50%","style":{"spacing":{"padding":{"left":"var:preset|spacing|40","right":"var:preset|spacing|40"}}}} -->
-<div class="wp-block-column is-vertically-aligned-center" style="padding-right:var(--wp--preset--spacing--40);padding-left:var(--wp--preset--spacing--40);flex-basis:50%"><!-- wp:image {"id":12081,"sizeSlug":"full","linkDestination":"none"} -->
-<figure class="wp-block-image size-full"><img src="https://i2.wp.com/wordpress.org/files/2022/09/mobile-illustration.png?fit=1148%2C614&amp;ssl=1" alt="" class="wp-image-12081" /></figure>
+<!-- wp:spacer {"height":"40px"} -->
+<div style="height:40px" aria-hidden="true" class="wp-block-spacer"></div>
+<!-- /wp:spacer -->
+
+<!-- wp:image {"align":"center","id":12081,"sizeSlug":"full","linkDestination":"none"} -->
+<figure class="wp-block-image aligncenter size-full"><img src="https://i2.wp.com/wordpress.org/files/2022/09/mobile-illustration.png?fit=1148%2C614&amp;ssl=1" alt="" class="wp-image-12081" /></figure>
 <!-- /wp:image --></div>
-<!-- /wp:column --></div>
-<!-- /wp:columns --></div></div>
-<!-- /wp:cover --></div>
-<!-- /wp:group -->
+<!-- /wp:group --></div></div>
+<!-- /wp:cover -->
