e7b676992f650b449892be662841fd60be92ce88
Kelly Dwan <ryelle@users.noreply.github.com>
2023-06-21 13:32:34 -0400

    Templates: Add the local nav banner to header template-part (#271)
    This is called by the index, archive, and single templates provided by the parent theme. These are not used on the main (English) site, but they are used on the Rosetta sites.

---
+++ source/wp-content/themes/wporg-main-2022/patterns/_nav-index.php
@@ -0,0 +1,16 @@
+<?php
+// phpcs:disable WordPress.Files.FileName -- Allow underscore for pattern partial.
+/**
+ * Title: Section Nav (Index (all posts))
+ * Slug: wporg-main-2022/nav-index
+ * Inserter: no
+ */
+
+?>
+<!-- wp:group {"align":"full","style":{"border":{"bottom":{"color":"var:preset|color|light-grey-1","width":"1px"}},"spacing":{"padding":{"right":"var:preset|spacing|60","left":"var:preset|spacing|60","top":"16px","bottom":"16px"}}},"backgroundColor":"white","className":"is-sticky","layout":{"type":"constrained"}} -->
+<div class="wp-block-group alignfull is-sticky has-white-background-color has-background" style="border-bottom-color:var(--wp--preset--color--light-grey-1);border-bottom-width:1px;padding-top:16px;padding-right:var(--wp--preset--spacing--60);padding-bottom:16px;padding-left:var(--wp--preset--spacing--60)"><!-- wp:group {"align":"full","layout":{"type":"flex","flexWrap":"wrap","justifyContent":"space-between"}} -->
+<div class="wp-block-group alignfull"><!-- wp:paragraph {"style":{"typography":{"fontStyle":"normal","fontWeight":"700"}},"fontSize":"small"} -->
+<p class="has-small-font-size" style="font-style:normal;font-weight:700"><?php _e( 'News', 'wporg' ); ?></p>
+<!-- /wp:paragraph --></div>
+<!-- /wp:group --></div>
+<!-- /wp:group -->
