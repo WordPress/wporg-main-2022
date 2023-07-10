8d894c9219745e892ae0084974c407244220f19f
Kelly Dwan <ryelle@users.noreply.github.com>
2023-07-10 16:56:32 -0400

    Content: Add some starter content to the page to prevent linter error

--- source/wp-content/themes/wporg-main-2022/patterns/download-releases-6-3.php
+++ source/wp-content/themes/wporg-main-2022/patterns/download-releases-6-3.php
@@ -6,4 +6,12 @@
  */
 
 ?>
+<!-- wp:group {"align":"full","style":{"spacing":{"padding":{"top":"var:preset|spacing|60","right":"var:preset|spacing|edge-space","left":"var:preset|spacing|edge-space","bottom":"var:preset|spacing|60"}}},"layout":{"type":"constrained"}} -->
+<div class="wp-block-group alignfull" style="padding-top:var(--wp--preset--spacing--60);padding-right:var(--wp--preset--spacing--edge-space);padding-bottom:var(--wp--preset--spacing--60);padding-left:var(--wp--preset--spacing--edge-space)"><!-- wp:heading {"level":1,"style":{"spacing":{"margin":{"bottom":"var:preset|spacing|30"}}}} -->
+<h1 class="wp-block-heading" style="margin-bottom:var(--wp--preset--spacing--30)"><?php _e( 'WordPress 6.3', 'wporg' ); ?></h1>
+<!-- /wp:heading -->
 
+<!-- wp:paragraph -->
+<p><?php _e( 'Content…', 'wporg' ); ?></p>
+<!-- /wp:paragraph --></div>
+<!-- /wp:group -->
