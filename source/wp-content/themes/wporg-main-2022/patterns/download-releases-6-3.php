6dc0d304dd1831c6fd0734e218c22a097eb47beb
Kelly Dwan <ryelle@users.noreply.github.com>
2023-07-12 13:32:06 -0400

    Content: Update template for 6.3 page
    See #282

--- source/wp-content/themes/wporg-main-2022/patterns/download-releases-6-3.php
+++ source/wp-content/themes/wporg-main-2022/patterns/download-releases-6-3.php
@@ -6,12 +6,10 @@
  */
 
 ?>
-<!-- wp:group {"align":"full","style":{"spacing":{"padding":{"top":"var:preset|spacing|60","right":"var:preset|spacing|edge-space","left":"var:preset|spacing|edge-space","bottom":"var:preset|spacing|60"}}},"layout":{"type":"constrained"}} -->
-<div class="wp-block-group alignfull" style="padding-top:var(--wp--preset--spacing--60);padding-right:var(--wp--preset--spacing--edge-space);padding-bottom:var(--wp--preset--spacing--60);padding-left:var(--wp--preset--spacing--edge-space)"><!-- wp:heading {"level":1,"style":{"spacing":{"margin":{"bottom":"var:preset|spacing|30"}}}} -->
-<h1 class="wp-block-heading" style="margin-bottom:var(--wp--preset--spacing--30)"><?php _e( 'WordPress 6.3', 'wporg' ); ?></h1>
-<!-- /wp:heading -->
-
-<!-- wp:paragraph -->
-<p><?php _e( 'Content…', 'wporg' ); ?></p>
+<!-- wp:cover {"customOverlayColor":"#151515","minHeight":70,"minHeightUnit":"vh","contentPosition":"center center","align":"full","style":{"spacing":{"padding":{"top":"var:preset|spacing|edge-space","right":"var:preset|spacing|edge-space","bottom":"var:preset|spacing|edge-space","left":"var:preset|spacing|edge-space"}},"color":{"text":"#ffffffa6"}}} -->
+<div class="wp-block-cover alignfull has-text-color" style="color:#ffffffa6;padding-top:var(--wp--preset--spacing--edge-space);padding-right:var(--wp--preset--spacing--edge-space);padding-bottom:var(--wp--preset--spacing--edge-space);padding-left:var(--wp--preset--spacing--edge-space);min-height:70vh"><span aria-hidden="true" class="wp-block-cover__background has-background-dim-100 has-background-dim" style="background-color:#151515"></span><div class="wp-block-cover__inner-container"><!-- wp:group {"layout":{"type":"constrained","contentSize":"900px"}} -->
+<div class="wp-block-group"><!-- wp:paragraph {"align":"center","style":{"typography":{"lineHeight":"1.4"}},"fontSize":"large"} -->
+<p class="has-text-align-center has-large-font-size" style="line-height:1.4">Coming soon</p>
 <!-- /wp:paragraph --></div>
-<!-- /wp:group -->
+<!-- /wp:group --></div></div>
+<!-- /wp:cover -->
