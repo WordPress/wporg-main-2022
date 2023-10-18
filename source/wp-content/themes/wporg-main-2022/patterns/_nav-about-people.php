11b3e47bbf839ee874ec8116e4147b35a84d122a
Adam Wood <1017872+adamwoodnz@users.noreply.github.com>
2023-10-18 15:53:05 +1300

    Use menu icon for local navs

--- source/wp-content/themes/wporg-main-2022/patterns/_nav-about-people.php
+++ source/wp-content/themes/wporg-main-2022/patterns/_nav-about-people.php
@@ -10,5 +10,5 @@
 <!-- wp:wporg/local-navigation-bar {"backgroundColor":"white","style":{"spacing":{"padding":{"right":"var:preset|spacing|edge-space","left":"var:preset|spacing|edge-space","top":"16px","bottom":"16px"}},"position":{"type":"sticky"},"border":{"top":{"color":"var:preset|color|light-grey-1","style":"solid","width":"1px"},"right":{},"bottom":{"color":"var:preset|color|light-grey-1","style":"solid","width":"1px"},"left":{}}},"textColor":"charcoal-1","fontSize":"small"} -->
 <!-- wp:site-title {"level":0,"fontSize":"small"} /-->
 
-<!-- wp:navigation {"menuSlug":"about-people","textColor":"blueberry-1","hasIcon":false,"layout":{"type":"flex","orientation":"horizontal"},"fontSize":"small"} /-->
+<!-- wp:navigation {"menuSlug":"about-people","textColor":"blueberry-1","icon":"menu","layout":{"type":"flex","orientation":"horizontal"},"fontSize":"small"} /-->
 <!-- /wp:wporg/local-navigation-bar -->
