6bab98ce50c4a8a15c51cb3b0b980d885ec635bc
StevenDufresne <dufresnesteven@gmail.com>
2023-11-08 15:10:19 +0900

    Add a control to allow columns to be changed.

--- source/wp-content/themes/wporg-main-2022/src/remembers-list/edit.js
+++ source/wp-content/themes/wporg-main-2022/src/remembers-list/edit.js
@@ -2,13 +2,26 @@
  * WordPress dependencies
  */
 
-import { useBlockProps } from '@wordpress/block-editor';
-import { Disabled } from '@wordpress/components';
+import { Disabled, PanelBody, RangeControl } from '@wordpress/components';
 import ServerSideRender from '@wordpress/server-side-render';
+import { InspectorControls, useBlockProps } from '@wordpress/block-editor';
+import { __ } from '@wordpress/i18n';
 
-export default function Edit( { attributes, name } ) {
+export default function Edit( { attributes, name, setAttributes } ) {
+	const { columns } = attributes;
 	return (
 		<div { ...useBlockProps() }>
+			<InspectorControls>
+				<PanelBody title={ __( 'Settings', 'wporg' ) }>
+					<RangeControl
+						label={ __( 'Columns', 'wporg' ) }
+						value={ columns }
+						onChange={ ( newNumber ) => setAttributes( { columns: parseInt( newNumber ) } ) }
+						min={ Math.max( 1, 1 ) }
+						max={ Math.max( 6, 10 ) }
+					/>
+				</PanelBody>
+			</InspectorControls>
 			<Disabled>
 				<ServerSideRender block={ name } attributes={ attributes } />
 			</Disabled>
