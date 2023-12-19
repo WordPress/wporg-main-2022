59082d0bea856f9f3acd4f679c8e7a7215e66f25
Adam Wood <1017872+adamwoodnz@users.noreply.github.com>
2023-12-19 15:57:49 +1300

    Fix incorrectly positioned sticky header on search page
    Use local nav bar instead of custom styled group
    
    Fixes https://github.com/WordPress/wporg-main-2022/issues/373

--- source/wp-content/themes/wporg-main-2022/patterns/search.php
+++ source/wp-content/themes/wporg-main-2022/patterns/search.php
@@ -6,16 +6,16 @@
  */
 
 ?>
-<!-- wp:group {"align":"full","style":{"elements":{"link":{"color":{"text":"var:preset|color|white"}}},"spacing":{"padding":{"top":"16px","right":"var:preset|spacing|60","bottom":"0","left":"var:preset|spacing|60"}}},"backgroundColor":"blueberry-1","textColor":"white","className":"is-style-brush-stroke is-sticky","layout":{"type":"constrained"}} -->
-<div class="wp-block-group alignfull is-style-brush-stroke is-sticky has-white-color has-blueberry-1-background-color has-text-color has-background has-link-color" style="padding-top:16px;padding-right:var(--wp--preset--spacing--60);padding-bottom:0;padding-left:var(--wp--preset--spacing--60)"><!-- wp:group {"align":"full","layout":{"type":"flex","flexWrap":"wrap","justifyContent":"space-between"}} -->
-<div class="wp-block-group alignfull"><!-- wp:paragraph {"style":{"typography":{"fontStyle":"normal","fontWeight":"700"}},"fontSize":"small"} -->
-<p class="has-small-font-size" style="font-style:normal;font-weight:700"><?php _e( 'Search', 'wporg' ); ?></p>
-<!-- /wp:paragraph --></div>
-<!-- /wp:group --></div>
-<!-- /wp:group -->
+<!-- wp:wporg/local-navigation-bar {"className":"is-style-brush-stroke","style":{"position":{"type":"sticky"}},"fontSize":"small"} -->
+	<!-- wp:paragraph {"style":{"typography":{"fontStyle":"normal"}},"fontSize":"small"} -->
+	<p class="has-small-font-size" style="font-style:normal"><?php _e( 'Search', 'wporg' ); ?></p>
+	<!-- /wp:paragraph -->
+<!-- /wp:wporg/local-navigation-bar -->
 
 <!-- wp:group {"align":"full","style":{"spacing":{"padding":{"top":"var:preset|spacing|50"}}},"layout":{"type":"constrained"}} -->
-<div class="wp-block-group alignfull" style="padding-top:var(--wp--preset--spacing--50)"><!-- wp:wporg/google-search-embed {"align":"wide"} /--></div>
+<div class="wp-block-group alignfull" style="padding-top:var(--wp--preset--spacing--50)">
+	<!-- wp:wporg/google-search-embed {"align":"wide"} /-->
+</div>
 <!-- /wp:group -->
 
 <!-- wp:spacer {"height":"40px"} -->
