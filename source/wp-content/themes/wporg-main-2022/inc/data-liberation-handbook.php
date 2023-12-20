05fadc6d07c6b58f3200f572688dee2c78e20bdc
Dion Hulse <dion@wordpress.org>
2023-12-20 16:17:24 +1000

    Create the Data Liberation handbook importer.
    This requires that the site be running the 'Handbook' and 'Wporg Markdown' plugins.

---
+++ source/wp-content/themes/wporg-main-2022/inc/data-liberation-handbook.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace WordPressdotorg\Theme\Main_2022;
+
+defined( 'WPINC' ) || die();
+
+add_filter( 'handbooks_config', __NAMESPACE__ . '\add_data_liberation_handbook' );
+
+/**
+ * Configures the Data liberation "handbook" (guides).
+ *
+ * Requires the site to be running the Handbook & WPORG Markdown plugins.
+ */
+function add_data_liberation_handbook( $handbooks ) {
+	$handbooks['and'] = [
+		'label'    => 'Data Liberation Guides',
+		'manifest' => 'https://raw.githubusercontent.com/WordPress/data-liberation/add/manifest/assets/manifest.json', // TODO: See https://github.com/WordPress/data-liberation/pull/46
+		'slug'     => 'and',
+	];
+
+	return $handbooks;
+} );
