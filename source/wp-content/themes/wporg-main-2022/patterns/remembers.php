6089bf8724d92781e487408f7820a89d9800b810
Adam Wood <1017872+adamwoodnz@users.noreply.github.com>
2023-12-21 11:18:36 +1300

    Sync Remembers page pattern

--- source/wp-content/themes/wporg-main-2022/patterns/remembers.php
+++ source/wp-content/themes/wporg-main-2022/patterns/remembers.php
@@ -12,7 +12,7 @@
 
 <!-- wp:group {"align":"wide","style":{"spacing":{"padding":{"bottom":"var:preset|spacing|30","right":"var:preset|spacing|30","left":"var:preset|spacing|30"},"margin":{"bottom":"0"}}},"layout":{"type":"constrained"}} -->
 <div class="wp-block-group alignwide" style="margin-bottom:0;padding-right:var(--wp--preset--spacing--30);padding-bottom:var(--wp--preset--spacing--30);padding-left:var(--wp--preset--spacing--30)"><!-- wp:paragraph -->
-<p><?php _e( 'WordPress dedicates this page to the memory of those we&#039;ve lost. They&#039;ve shaped our project and enriched our community. As we remember their passion and commitment to WordPress and open source software, we celebrate their spirit.', 'wporg' ); ?></p>
+<p><?php _e( 'WordPress dedicates this page to the memory of those we’ve lost. They’ve shaped our project and enriched our community. As we remember their passion and commitment to WordPress and open source software, we celebrate their spirit.', 'wporg' ); ?></p>
 <!-- /wp:paragraph -->
 
 <!-- wp:paragraph -->
@@ -21,16 +21,13 @@
 <!-- /wp:group -->
 
 <!-- wp:group {"align":"wide","style":{"spacing":{"margin":{"top":"var:preset|spacing|30","bottom":"var:preset|spacing|30"}}},"layout":{"type":"constrained"}} -->
-<div class="wp-block-group alignwide" style="margin-top:var(--wp--preset--spacing--30);margin-bottom:var(--wp--preset--spacing--30)">
-<!-- wp:paragraph {"align":"center","fontSize":"medium"} -->
+<div class="wp-block-group alignwide" style="margin-top:var(--wp--preset--spacing--30);margin-bottom:var(--wp--preset--spacing--30)"><!-- wp:paragraph {"align":"center","fontSize":"medium"} -->
 <p class="has-text-align-center has-medium-font-size">❦</p>
 <!-- /wp:paragraph --></div>
 <!-- /wp:group -->
 
 <!-- wp:group {"align":"wide","style":{"spacing":{"padding":{"bottom":"var:preset|spacing|40","right":"var:preset|spacing|30","left":"var:preset|spacing|30"},"margin":{"bottom":"0"}}},"layout":{"type":"constrained"}} -->
-<div class="wp-block-group alignwide" style="margin-bottom:0;padding-right:var(--wp--preset--spacing--30);padding-bottom:var(--wp--preset--spacing--40);padding-left:var(--wp--preset--spacing--30)">
-<!-- wp:wporg/remembers-list {"columns":3} /-->
-</div>
+<div class="wp-block-group alignwide" style="margin-bottom:0;padding-right:var(--wp--preset--spacing--30);padding-bottom:var(--wp--preset--spacing--40);padding-left:var(--wp--preset--spacing--30)"><!-- wp:wporg/remembers-list /-->
 
 <!-- wp:paragraph {"align":"center","fontSize":"medium"} -->
 <p class="has-text-align-center has-medium-font-size">❦</p>
@@ -40,4 +37,5 @@
 <div class="wp-block-group alignwide" style="margin-bottom:0;padding-right:var(--wp--preset--spacing--30);padding-bottom:var(--wp--preset--spacing--40);padding-left:var(--wp--preset--spacing--30)"><!-- wp:paragraph {"align":"left","style":{"elements":{"link":{"color":{"text":"var:preset|color|charcoal-1"}}},"spacing":{"margin":{"top":"var:preset|spacing|50","right":"var:preset|spacing|default","bottom":"var:preset|spacing|default","left":"var:preset|spacing|default"}}}} -->
 <p class="has-text-align-left has-link-color" style="margin-top:var(--wp--preset--spacing--50);margin-right:var(--wp--preset--spacing--default);margin-bottom:var(--wp--preset--spacing--default);margin-left:var(--wp--preset--spacing--default)"><?php _e( 'This memorial is volunteer maintained and managed. To add a tribute for a lost contributor please contact us: <a href="mailto:memorials@wordpress.org">memorials@wordpress.org</a>', 'wporg' ); ?></p>
 <!-- /wp:paragraph --></div>
+<!-- /wp:group --></div>
 <!-- /wp:group -->
