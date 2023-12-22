7d3f4191caebefa97b7e23e513504a781ea081a3
StevenDufresne <dufresnesteven@gmail.com>
2023-12-22 09:48:54 +0900

    Remove the links in titles.

--- source/wp-content/themes/wporg-main-2022/functions.php
+++ source/wp-content/themes/wporg-main-2022/functions.php
@@ -26,6 +26,12 @@ add_filter( 'wporg_block_site_breadcrumbs', __NAMESPACE__ . '\update_site_breadc
 add_filter( 'render_block_core/site-title', __NAMESPACE__ . '\use_parent_page_title', 10, 3 );
 add_filter( 'render_block_data', __NAMESPACE__ . '\update_header_template_part_class' );
 add_filter( 'wporg_block_navigation_menus', __NAMESPACE__ . '\add_site_navigation_menus' );
+
+
+// Remove table of contents.
+add_filter( 'wporg_handbook_toc_should_add_toc', '__return_false' );
+
+// Remove the edit link from handbook titles.
 add_filter( 'wporg_markdown_should_filter_title', '__return_false' );
 
 /**
