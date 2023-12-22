bce74bfe6d99a3064e8fcfa543fd7e9a0ad75d3f
StevenDufresne <dufresnesteven@gmail.com>
2023-12-22 11:44:45 +0900

    Add a callout on the guides page.

--- source/wp-content/themes/wporg-main-2022/patterns/data-liberation-guides.php
+++ source/wp-content/themes/wporg-main-2022/patterns/data-liberation-guides.php
@@ -7,31 +7,31 @@
 
 ?>
 
-<!-- wp:group {"align":"wide","style":{"spacing":{"padding":{"top":"var:preset|spacing|60","bottom":"var:preset|spacing|80"}}},"layout":{"type":"default"}} -->
+<!-- wp:group {"align":"wide","style":{"spacing":{"padding":{"right":"var:preset|spacing|edge-space","left":"var:preset|spacing|edge-space"}}},"layout":{"type":"constrained"}} -->
+<div class="wp-block-group alignwide" style="padding-right:var(--wp--preset--spacing--edge-space);padding-left:var(--wp--preset--spacing--edge-space)"><!-- wp:group {"align":"wide","style":{"spacing":{"padding":{"top":"var:preset|spacing|60","bottom":"var:preset|spacing|80"}}},"layout":{"type":"default"}} -->
 <div class="wp-block-group alignwide" style="padding-top:var(--wp--preset--spacing--60);padding-bottom:var(--wp--preset--spacing--80)"><!-- wp:heading {"style":{"spacing":{"margin":{"bottom":"var:preset|spacing|30","top":"0"}}}} -->
 <h2 class="wp-block-heading" style="margin-top:0;margin-bottom:var(--wp--preset--spacing--30)">Guides</h2>
 <!-- /wp:heading -->
 
-<!-- wp:group {"align":"wide","className":"wporg-events__filters","style":{"spacing":{"margin":{"top":"40px","bottom":"40px"}}},"layout":{"type":"flex","flexWrap":"wrap","justifyContent":"space-between"}} -->
-<div class="wp-block-group alignwide wporg-events__filters" style="margin-top:40px;margin-bottom:40px">
-	<!-- wp:group {"className":"wporg-events__filters__search","layout":{"type":"flex","flexWrap":"wrap"}} -->
-	<div class="wp-block-group wporg-events__filters__search">
-		<!-- wp:search {"showLabel":false,"placeholder":"Search guides...","width":100,"widthUnit":"%","buttonText":"Search","buttonPosition":"button-inside","buttonUseIcon":true,"className":"is-style-secondary-search-control"} /-->
+<!-- wp:group {"align":"wide","style":{"spacing":{"margin":{"top":"40px","bottom":"40px"}}},"className":"wporg-events__filters","layout":{"type":"flex","flexWrap":"wrap","justifyContent":"space-between"}} -->
+<div class="wp-block-group alignwide wporg-events__filters" style="margin-top:40px;margin-bottom:40px"><!-- wp:group {"className":"wporg-events__filters__search","layout":{"type":"flex","flexWrap":"wrap"}} -->
+<div class="wp-block-group wporg-events__filters__search"><!-- wp:search {"showLabel":false,"placeholder":"Search guides...","width":100,"widthUnit":"%","buttonText":"Search","buttonPosition":"button-inside","buttonUseIcon":true,"className":"is-style-secondary-search-control"} /-->
 
-		<!-- wp:query {"queryId":0,"query":{"perPage":500,"pages":0,"offset":0,"postType":"and-handbook","order":"desc","orderBy":"date","author":"","search":"","exclude":[],"sticky":"","inherit":false,"parents":[]}} -->
-			<!-- wp:wporg/query-total /-->
-		<!-- /wp:query -->
-	</div> <!-- /wp:group -->
+<!-- wp:query {"queryId":0,"query":{"perPage":500,"pages":0,"offset":0,"postType":"and-handbook","order":"desc","orderBy":"date","author":"","search":"","exclude":[],"sticky":"","inherit":false,"parents":[]}} -->
+<div class="wp-block-query"><!-- wp:wporg/query-total /--></div>
+<!-- /wp:query --></div>
+<!-- /wp:group -->
 
-	<!-- wp:group {"style":{"spacing":{"blockGap":"0"}},"className":"wporg-query-filters","layout":{"type":"flex","flexWrap":"nowrap"}} -->
-	<div class="wp-block-group wporg-query-filters">
-		<!-- wp:wporg/query-filter {"key":"format_type","multiple":false} /-->
-		<!-- wp:wporg/query-filter {"key":"event_type","multiple":true} /-->
-		<!-- wp:wporg/query-filter {"key":"month","multiple":true} /-->
-		<!-- wp:wporg/query-filter {"key":"country","multiple":true} /-->
-	</div> <!-- /wp:group -->
+<!-- wp:group {"style":{"spacing":{"blockGap":"0"}},"className":"wporg-query-filters","layout":{"type":"flex","flexWrap":"nowrap"}} -->
+<div class="wp-block-group wporg-query-filters"><!-- wp:wporg/query-filter {"key":"format_type","multiple":false} /-->
 
-</div> <!-- /wp:group -->
+<!-- wp:wporg/query-filter {"key":"event_type"} /-->
+
+<!-- wp:wporg/query-filter {"key":"month"} /-->
+
+<!-- wp:wporg/query-filter {"key":"country"} /--></div>
+<!-- /wp:group --></div>
+<!-- /wp:group -->
 
 <!-- wp:group {"style":{"border":{"bottom":{"color":"var:preset|color|light-grey-1","width":"1px"},"top":[],"right":[],"left":[]}},"layout":{"type":"default"}} -->
 <div class="wp-block-group" style="border-bottom-color:var(--wp--preset--color--light-grey-1);border-bottom-width:1px"><!-- wp:query {"queryId":2,"query":{"perPage":10,"pages":0,"offset":0,"postType":"and-handbook","order":"desc","orderBy":"date","author":"","search":"","exclude":[],"sticky":"","inherit":false},"layout":{"type":"default"}} -->
@@ -51,5 +51,20 @@
 <!-- wp:query-pagination-next /-->
 <!-- /wp:query-pagination --></div>
 <!-- /wp:query --></div>
+<!-- /wp:group --></div>
+<!-- /wp:group --></div>
+<!-- /wp:group -->
+
+<!-- wp:group {"align":"full","style":{"elements":{"link":{"color":{"text":"var:preset|color|white"}}},"spacing":{"padding":{"top":"5rem","right":"var:preset|spacing|edge-space","bottom":"5rem","left":"var:preset|spacing|edge-space"}}},"backgroundColor":"blueberry-1","textColor":"white","layout":{"inherit":true,"type":"constrained"}} -->
+<div class="wp-block-group alignfull has-white-color has-blueberry-1-background-color has-text-color has-background has-link-color" id="get-started" style="padding-top:5rem;padding-right:var(--wp--preset--spacing--edge-space);padding-bottom:5rem;padding-left:var(--wp--preset--spacing--edge-space)"><!-- wp:group {"align":"wide","layout":{"type":"flex","flexWrap":"nowrap"}} -->
+<div class="wp-block-group alignwide"><!-- wp:paragraph {"style":{"layout":{"selfStretch":"fixed","flexSize":"65%"},"elements":{"link":{"color":{"text":"var:preset|color|white"}}}},"textColor":"white","fontSize":"heading-4","fontFamily":"eb-garamond"} -->
+<p class="has-white-color has-text-color has-link-color has-eb-garamond-font-family has-heading-4-font-size">Have a workflow, tool, or time to contribute? We'd love help to make the web more open.</p>
+<!-- /wp:paragraph -->
+
+<!-- wp:buttons {"layout":{"type":"flex","justifyContent":"right","orientation":"horizontal"},"style":{"layout":{"selfStretch":"fill","flexSize":null}}} -->
+<div class="wp-block-buttons"><!-- wp:button {"className":"has-custom-width is-style-outline-on-dark"} -->
+<div class="wp-block-button has-custom-width is-style-outline-on-dark"><a class="wp-block-button__link wp-element-button" href="https://github.com/WordPress/move-to-wp/tree/trunk#how-to-contribute">Get Started</a></div>
+<!-- /wp:button --></div>
+<!-- /wp:buttons --></div>
 <!-- /wp:group --></div>
 <!-- /wp:group -->
