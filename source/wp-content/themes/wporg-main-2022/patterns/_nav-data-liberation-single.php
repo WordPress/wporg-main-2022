462548bf5ccb5852826871a9b62b15f134318898
StevenDufresne <dufresnesteven@gmail.com>
2024-01-22 16:05:23 +0900

    Clean up the template.

--- source/wp-content/themes/wporg-main-2022/patterns/_nav-data-liberation-single.php
+++ source/wp-content/themes/wporg-main-2022/patterns/_nav-data-liberation-single.php
@@ -12,7 +12,5 @@
 
 <!-- wp:group {"className":"wporg-breadcrumbs","align":"full","style":{"spacing":{"padding":{"top":"18px","bottom":"18px","left":"var:preset|spacing|edge-space","right":"var:preset|spacing|edge-space"}}},"backgroundColor":"white","layout":{"type":"flex","flexWrap":"wrap","justifyContent":"space-between"}} -->
 <div class="wporg-breadcrumbs wp-block-group alignfull has-white-background-color has-background" style="padding-top:18px;padding-right:var(--wp--preset--spacing--edge-space);padding-bottom:18px;padding-left:var(--wp--preset--spacing--edge-space)">
-	
 	<!-- wp:wporg/site-breadcrumbs {"fontSize":"small"} /-->
-
 </div>
