ff2f7c8641908fd0006c3382ce97053cc5d742f1
StevenDufresne <dufresnesteven@gmail.com>
2024-01-22 15:23:56 +0900

    Remove the post details.

--- source/wp-content/themes/wporg-main-2022/patterns/data-liberation-guides-single.php
+++ source/wp-content/themes/wporg-main-2022/patterns/data-liberation-guides-single.php
@@ -8,30 +8,12 @@
 ?>
 <!-- wp:group {"align":"wide","layout":{"type":"constrained","justifyContent":"left"}} -->
 <div class="wp-block-group alignwide"><!-- wp:group {"tagName":"article"} -->
-<article class="wp-block-group"><!-- wp:post-title {"level":1,"style":{"spacing":{"margin":{"top":"var:preset|spacing|edge-space","bottom":"var:preset|spacing|40"}}}} /-->
+<article class="wp-block-group"><!-- wp:post-title {"level":1,"style":{"spacing":{"margin":{"top":"var:preset|spacing|40","bottom":"var:preset|spacing|40"}}}} /-->
 
 <!-- wp:wporg/sidebar-container {"style":{"spacing":{"margin":{"bottom":"40px"}}}} -->
 <!-- wp:wporg/table-of-contents /-->
 <!-- /wp:wporg/sidebar-container -->
 
 <!-- wp:post-content {"layout":{"inherit":true}} /--></article>
-<!-- /wp:group -->
-
-<!-- wp:group {"style":{"spacing":{"padding":{"top":"var:preset|spacing|40"},"margin":{"top":"var:preset|spacing|40","bottom":"var:preset|spacing|40"},"blockGap":"var:preset|spacing|50"},"border":{"top":{"color":"var:preset|color|light-grey-1","width":"1px"}}},"layout":{"type":"flex","flexWrap":"nowrap","justifyContent":"left"}} -->
-<div class="wp-block-group" style="border-top-color:var(--wp--preset--color--light-grey-1);border-top-width:1px;margin-top:var(--wp--preset--spacing--40);margin-bottom:var(--wp--preset--spacing--40);padding-top:var(--wp--preset--spacing--40)"><!-- wp:group {"style":{"spacing":{"blockGap":"0"}},"layout":{"type":"constrained"}} -->
-<div class="wp-block-group"><!-- wp:paragraph {"style":{"typography":{"fontStyle":"normal","fontWeight":"700"}}} -->
-<p style="font-style:normal;font-weight:700"><?php _e( 'First published', 'wporg' ); ?></p>
-<!-- /wp:paragraph -->
-
-<!-- wp:post-date {"fontSize":"normal"} /--></div>
-<!-- /wp:group -->
-
-<!-- wp:group {"style":{"spacing":{"blockGap":"0"}},"layout":{"type":"constrained"}} -->
-<div class="wp-block-group"><!-- wp:paragraph {"style":{"typography":{"fontStyle":"normal","fontWeight":"700"}}} -->
-<p style="font-style:normal;font-weight:700"><?php _e( 'Last updated', 'wporg' ); ?></p>
-<!-- /wp:paragraph -->
-
-<!-- wp:post-date {"displayType":"modified","fontSize":"normal"} /--></div>
-<!-- /wp:group --></div>
 <!-- /wp:group --></div>
 <!-- /wp:group -->
