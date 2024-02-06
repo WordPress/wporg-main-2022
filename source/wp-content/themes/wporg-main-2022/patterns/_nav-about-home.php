2a019558aa69f5838a2d862378ccdfd52f66faae
Kelly Dwan <ryelle@users.noreply.github.com>
2024-02-06 18:04:06 -0500

    Remove self-link on About page

--- source/wp-content/themes/wporg-main-2022/patterns/_nav-about-home.php
+++ source/wp-content/themes/wporg-main-2022/patterns/_nav-about-home.php
@@ -8,7 +8,7 @@
 
 ?>
 <!-- wp:wporg/local-navigation-bar {"style":{"color":{"background":"#f0ede7"},"border":{"bottom":{"color":"var:preset|color|black-opacity-15","width":"1px"}},"spacing":{"padding":{"right":"var:preset|spacing|edge-space","left":"var:preset|spacing|edge-space","top":"16px","bottom":"16px"}},"position":{"type":"sticky"}},"textColor":"charcoal-1","fontSize":"small"} -->
-<!-- wp:site-title {"level":0,"fontSize":"small"} /-->
+<!-- wp:site-title {"level":0,"fontSize":"small","isLink":false} /-->
 
 <!-- wp:navigation {"menuSlug":"about-home","textColor":"charcoal-1","icon":"menu","customOverlayBackgroundColor":"#f0ede7","layout":{"type":"flex","orientation":"horizontal"},"fontSize":"small","openSubmenusOnClick":true} /-->
 <!-- /wp:wporg/local-navigation-bar -->
