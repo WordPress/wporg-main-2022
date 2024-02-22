53cb3d2863dabc13acfef86a794f530851ec0b6d
Kelly Dwan <ryelle@users.noreply.github.com>
2024-02-22 12:57:12 -0500

    About Subpages: Show the page title on scroll, update links to use charcoal color.
    See https://github.com/WordPress/wporg-mu-plugins/issues/535

--- source/wp-content/themes/wporg-main-2022/patterns/_nav-about-subpage.php
+++ source/wp-content/themes/wporg-main-2022/patterns/_nav-about-subpage.php
@@ -8,7 +8,13 @@
 
 ?>
 <!-- wp:wporg/local-navigation-bar {"backgroundColor":"white","style":{"spacing":{"padding":{"right":"var:preset|spacing|edge-space","left":"var:preset|spacing|edge-space","top":"16px","bottom":"16px"}},"position":{"type":"sticky"},"border":{"top":{},"right":{},"bottom":{"color":"var:preset|color|light-grey-1","style":"solid","width":"1px"},"left":{}}},"textColor":"charcoal-1","fontSize":"small"} -->
-    <!-- wp:site-title {"level":0,"fontSize":"small"} /-->    
+	<!-- wp:group {"style":{"spacing":{"blockGap":"0"}},"layout":{"type":"flex","flexWrap":"nowrap"}} -->
+	<div class="wp-block-group">
+		<!-- wp:site-title {"level":0,"fontSize":"small"} /-->
 
-<!-- wp:navigation {"menuSlug":"about-home","textColor":"blueberry-1","icon":"menu","layout":{"type":"flex","orientation":"horizontal"},"fontSize":"small","openSubmenusOnClick":true} /-->
+		<!-- wp:post-title {"level":0,"fontSize":"small","fontFamily":"inter","className":"wporg-local-navigation-bar__fade-in-scroll"} /-->
+	</div>
+	<!-- /wp:group -->
+
+	<!-- wp:navigation {"menuSlug":"about-home","textColor":"charcoal-1","icon":"menu","layout":{"type":"flex","orientation":"horizontal"},"fontSize":"small","openSubmenusOnClick":true} /-->
 <!-- /wp:wporg/local-navigation-bar -->
