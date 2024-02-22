c8372d91161c236847c4b439dbe74f524974fe66
Dion Hulse <dion@wordpress.org>
2024-02-22 16:54:30 +1000

    Add alt text to the SOTW image
    Fixes https://meta.trac.wordpress.org/ticket/7481

--- source/wp-content/themes/wporg-main-2022/patterns/home.php
+++ source/wp-content/themes/wporg-main-2022/patterns/home.php
@@ -10,7 +10,7 @@
 <div class="wp-block-group alignfull has-black-background-color has-background" style="padding-right:var(--wp--preset--spacing--edge-space);padding-left:var(--wp--preset--spacing--edge-space)"><!-- wp:columns {"align":"wide","style":{"spacing":{"padding":{"top":"var:preset|spacing|30","bottom":"var:preset|spacing|30"},"blockGap":{"left":"var:preset|spacing|60"}}}} -->
 <div class="wp-block-columns alignwide" style="padding-top:var(--wp--preset--spacing--30);padding-bottom:var(--wp--preset--spacing--30)"><!-- wp:column {"verticalAlignment":"center"} -->
 <div class="wp-block-column is-vertically-aligned-center"><!-- wp:image {"id":24157,"sizeSlug":"full","linkDestination":"custom","className":"is-resized"} -->
-<figure class="wp-block-image size-full is-resized"><a href="https://wordpress.org/state-of-the-word/"><img src="https://wordpress.org/files/2023/12/sotw-dotorg-drawer.png" alt="" class="wp-image-24157" /></a></figure>
+<figure class="wp-block-image size-full is-resized"><a href="https://wordpress.org/state-of-the-word/"><img src="https://wordpress.org/files/2023/12/sotw-dotorg-drawer.png" alt="<?php _e( 'Watch the State of the Word.', 'wporg' ); ?>" class="wp-image-24157" /></a></figure>
 <!-- /wp:image --></div>
 <!-- /wp:column -->
 
