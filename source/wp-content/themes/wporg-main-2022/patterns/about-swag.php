cc6c2e45001e1f155fb9354a8f7dffd7543a8547
Adam Wood <1017872+adamwoodnz@users.noreply.github.com>
2024-06-07 09:44:15 +1200

    Add Swag page template and pattern (#448)
    * Add Swag page template and pattern
    
    * Update swag content
    
    * Update design

---
+++ source/wp-content/themes/wporg-main-2022/patterns/about-swag.php
@@ -0,0 +1,21 @@
+<?php
+/**
+ * Title: Swag
+ * Slug: wporg-main-2022/swag
+ * Inserter: no
+ */
+
+?>
+<!-- wp:group {"align":"full","style":{"spacing":{"padding":{"top":"var:preset|spacing|60","right":"var:preset|spacing|edge-space","left":"var:preset|spacing|edge-space","bottom":"var:preset|spacing|60"}}},"layout":{"type":"constrained"}} -->
+<div class="wp-block-group alignfull" style="padding-top:var(--wp--preset--spacing--60);padding-right:var(--wp--preset--spacing--edge-space);padding-bottom:var(--wp--preset--spacing--60);padding-left:var(--wp--preset--spacing--edge-space)"><!-- wp:heading {"level":1,"style":{"typography":{"fontStyle":"normal","fontWeight":"700"}},"fontSize":"heading-6","fontFamily":"inter"} -->
+<h1 class="wp-block-heading has-inter-font-family has-heading-6-font-size" style="font-style:normal;font-weight:700"><?php _e( 'Swag store', 'wporg' ); ?></h1>
+<!-- /wp:heading -->
+
+<!-- wp:paragraph -->
+<p><?php _e( 'The Mercantile: WordPress Swag Store has closed its doors for now. Thanks for always wearing your love of WordPress for all to see, and for supporting the WordPress Foundation.', 'wporg' ); ?></p>
+<!-- /wp:paragraph -->
+
+<!-- wp:paragraph -->
+<p><?php _e( 'Looking for another way to find swag and connect with fellow WordPressers? Check out a&nbsp;<a href="https://events.wordpress.org/">WordPress event</a>&nbsp;near you.', 'wporg' ); ?></p>
+<!-- /wp:paragraph --></div>
+<!-- /wp:group -->
