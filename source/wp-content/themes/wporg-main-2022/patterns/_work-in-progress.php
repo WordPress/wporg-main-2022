65affafb2d15d6c1efb92a3fdbc5725e4a7ca50a
Kelly Dwan <ryelle@users.noreply.github.com>
2024-07-04 14:07:12 -0400

    Content: Add a pattern that can be used for "pages in progress"

---
+++ source/wp-content/themes/wporg-main-2022/patterns/_work-in-progress.php
@@ -0,0 +1,28 @@
+<?php
+// phpcs:disable WordPress.Files.FileName -- Allow underscore for pattern partial.
+/**
+ * Title: Page in progress
+ * Slug: wporg-main-2022/work-in-progress
+ * Inserter: no
+ *
+ * Use this pattern to prevent publicly showing pages that are not ready yet.
+ */
+
+?>
+<!-- wp:group {"metadata":{"name":"Page"},"align":"full","style":{"spacing":{"blockGap":"0"}},"layout":{"type":"constrained"}} -->
+<div class="wp-block-group alignfull">
+	<!-- wp:cover {"overlayColor":"light-grey-2","isUserOverlayColor":true,"isDark":false,"align":"full","style":{"spacing":{"padding":{"top":"var:preset|spacing|70","bottom":"var:preset|spacing|70"}}},"layout":{"type":"constrained"}} -->
+	<div class="wp-block-cover alignfull is-light" style="padding-top:var(--wp--preset--spacing--70);padding-bottom:var(--wp--preset--spacing--70)">
+		<span aria-hidden="true" class="wp-block-cover__background has-light-grey-2-background-color has-background-dim-100 has-background-dim"></span>
+		<div class="wp-block-cover__inner-container">
+			<!-- wp:heading {"textAlign":"center","level":1,"style":{"typography":{"fontStyle":"italic","fontWeight":"400"}}} -->
+			<h1 class="wp-block-heading has-text-align-center" style="font-style:italic;font-weight:400">Coming soon…</h1>
+			<!-- /wp:heading -->
+			<!-- wp:paragraph {"align":"center","placeholder":"Write title…","fontSize":"large"} -->
+			<p class="has-text-align-center has-large-font-size">WordPress 6.6 is due to be released on the 16th of July.<br>Check back then for all the great new changes!</p>
+			<!-- /wp:paragraph -->
+		</div>
+	</div>
+	<!-- /wp:cover -->
+</div>
+<!-- /wp:group -->
