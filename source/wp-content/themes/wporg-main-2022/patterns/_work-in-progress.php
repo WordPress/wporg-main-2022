339fdcf7ff963e9d4a33fcff4efa2e84204b1e30
Kelly Dwan <ryelle@users.noreply.github.com>
2024-07-06 12:38:46 -0400

    Update content, style

--- source/wp-content/themes/wporg-main-2022/patterns/_work-in-progress.php
+++ source/wp-content/themes/wporg-main-2022/patterns/_work-in-progress.php
@@ -9,20 +9,34 @@
  */
 
 ?>
-<!-- wp:group {"metadata":{"name":"Page"},"align":"full","style":{"spacing":{"blockGap":"0"}},"layout":{"type":"constrained"}} -->
-<div class="wp-block-group alignfull">
-	<!-- wp:cover {"overlayColor":"light-grey-2","isUserOverlayColor":true,"isDark":false,"align":"full","style":{"spacing":{"padding":{"top":"var:preset|spacing|70","bottom":"var:preset|spacing|70"}}},"layout":{"type":"constrained"}} -->
-	<div class="wp-block-cover alignfull is-light" style="padding-top:var(--wp--preset--spacing--70);padding-bottom:var(--wp--preset--spacing--70)">
-		<span aria-hidden="true" class="wp-block-cover__background has-light-grey-2-background-color has-background-dim-100 has-background-dim"></span>
-		<div class="wp-block-cover__inner-container">
-			<!-- wp:heading {"textAlign":"center","level":1,"style":{"typography":{"fontStyle":"italic","fontWeight":"400"}}} -->
-			<h1 class="wp-block-heading has-text-align-center" style="font-style:italic;font-weight:400">Coming soon…</h1>
-			<!-- /wp:heading -->
-			<!-- wp:paragraph {"align":"center","placeholder":"Write title…","fontSize":"large"} -->
-			<p class="has-text-align-center has-large-font-size">WordPress 6.6 is due to be released on the 16th of July.<br>Check back then for all the great new changes!</p>
-			<!-- /wp:paragraph -->
-		</div>
+<!-- wp:group {"align":"full","style":{"spacing":{"padding":{"top":"var:preset|spacing|70","bottom":"var:preset|spacing|70","left":"var:preset|spacing|edge-space","right":"var:preset|spacing|edge-space"}},"border":{"top":{"color":"var:preset|color|black-opacity-15","style":"solid","width":"1px"},"right":{"style":"none","width":"0px"},"bottom":{"style":"none","width":"0px"},"left":{"style":"none","width":"0px"}}},"className":"wporg-page-in-progress","layout":{"type":"constrained"}} -->
+<div class="wp-block-group alignfull wporg-page-in-progress" style="border-top-color:var(--wp--preset--color--black-opacity-15);border-top-style:solid;border-top-width:1px;border-right-style:none;border-right-width:0px;border-bottom-style:none;border-bottom-width:0px;border-left-style:none;border-left-width:0px;padding-top:var(--wp--preset--spacing--70);padding-right:var(--wp--preset--spacing--edge-space);padding-bottom:var(--wp--preset--spacing--70);padding-left:var(--wp--preset--spacing--edge-space)">
+	<!-- wp:group {"align":"wide","layout":{"type":"constrained","justifyContent":"left"}} -->
+	<div class="wp-block-group alignwide">
+		<!-- wp:heading {"level":1,"style":{"typography":{"fontStyle":"italic","fontWeight":"400"}}} -->
+		<h1 class="wp-block-heading" style="font-style:italic;font-weight:400">You&#8217;re looking for what&#8217;s new in WordPress</h1>
+		<!-- /wp:heading -->
+
+		<!-- wp:paragraph -->
+		<p>Go right to the source:</p>
+		<!-- /wp:paragraph -->
+
+		<!-- wp:list -->
+		<ul class="wp-block-list">
+			<!-- wp:list-item -->
+			<li><a href="https://events.wordpress.org/upcoming-events/">Upcoming WordPress events</a></li>
+			<!-- /wp:list-item -->
+
+			<!-- wp:list-item -->
+			<li><a href="https://wordpress.org/news/">WordPress News</a></li>
+			<!-- /wp:list-item -->
+
+			<!-- wp:list-item -->
+			<li><a href="https://developer.wordpress.org/news/">Developer Blog</a></li>
+			<!-- /wp:list-item -->
+		</ul>
+		<!-- /wp:list -->
 	</div>
-	<!-- /wp:cover -->
+	<!-- /wp:group -->
 </div>
 <!-- /wp:group -->
