2320ab6e3f09758cd10e9f1c4add7b76c5b951c1
Dion Hulse <contact@dd32.id.au>
2025-12-02 11:52:36 +1000

    Add contributor count.

--- source/wp-content/themes/wporg-main-2022/patterns/download-releases-6-9.php
+++ source/wp-content/themes/wporg-main-2022/patterns/download-releases-6-9.php
@@ -309,7 +309,7 @@
 <div class="wp-block-columns alignwide"><!-- wp:column {"width":"70%"} -->
 <div class="wp-block-column" style="flex-basis:70%"><!-- wp:group {"align":"wide","layout":{"type":"flex","orientation":"vertical"}} -->
 <div class="wp-block-group alignwide"><!-- wp:heading {"fontSize":"heading-1"} -->
-<h2 class="wp-block-heading has-heading-1-font-size"><?php _e( 'Thanks to the over <em>XXX contributors</em> who made this release', 'wporg' ); ?></h2>
+<h2 class="wp-block-heading has-heading-1-font-size"><?php _e( 'Thanks to the over <em>900 contributors</em> who made this release', 'wporg' ); ?></h2>
 <!-- /wp:heading -->
 
 <!-- wp:spacer {"height":"0px","style":{"layout":{"flexSize":"16px","selfStretch":"fixed"}}} -->
