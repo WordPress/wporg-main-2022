a62b92934080938e599d3d217775f761b6177150
Dion Hulse <contact@dd32.id.au>
2025-12-03 16:58:54 +1000

    Education Menu: Split the menu into only having the post title on the main education page.

--- source/wp-content/themes/wporg-main-2022/patterns/_nav-education-home.php
+++ source/wp-content/themes/wporg-main-2022/patterns/_nav-education-home.php
@@ -2,19 +2,13 @@
 // phpcs:disable WordPress.Files.FileName -- Allow underscore for pattern partial.
 /**
  * Title: Section Nav (Education)
- * Slug: wporg-main-2022/nav-education
+ * Slug: wporg-main-2022/nav-education-home
  * Inserter: no
  */
 
 ?>
 <!-- wp:wporg/local-navigation-bar {"backgroundColor":"charcoal-2","style":{"position":{"type":"sticky"}},"textColor":"light-grey-1","fontSize":"small"} -->
-	<!-- wp:group {"style":{"spacing":{"blockGap":"0"}},"layout":{"type":"flex","flexWrap":"nowrap"}} -->
-	<div class="wp-block-group">
-		<!-- wp:site-title {"level":0,"fontSize":"small"} /-->
-
-		<!-- wp:post-title {"level":0,"fontSize":"small","fontFamily":"inter","className":"wporg-local-navigation-bar__fade-in-scroll"} /-->
-	</div>
-	<!-- /wp:group -->
+	<!-- wp:post-title {"level":0,"fontSize":"small","fontFamily":"inter","className":"wporg-local-navigation-bar__fade-in-scroll"} /-->
 
 	<!-- wp:navigation {"menuSlug":"education","textColor":"light-grey-1","icon":"menu","customOverlayBackgroundColor":"var(--wp--preset--color--charcoal-2)","layout":{"type":"flex","orientation":"horizontal"},"fontSize":"small","openSubmenusOnClick":true} /-->
 <!-- /wp:wporg/local-navigation-bar -->
